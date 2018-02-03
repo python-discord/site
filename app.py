1b0dd9bcb244c5a5ea55108fb87cf12237044a98
Christopher Baklid <christopherb@unity3d.com>
2018-02-03 21:55:55 +0100

    adds a catchall for 404 and a healthcheck route

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 
 # Snekchek
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -14,6 +15,16 @@ app._secret_key = os.environ.get("WEBPAGE_SECRET_KEY")
 @app.route("/")
 def _index():
     return "Robots are taking over"
+
+
+@app.route("/healthcheck")
+def healthcheck():
+    return jsonify({"status":"ok"})
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "replace me with a template, 404 not found", 404
 
 
 if __name__ == '__main__':
