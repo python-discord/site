7aafb0ba76e3fdc533e61d910a7b77484fbc9dac
JoeBanks13 <joseph@josephbanks.me>
2018-02-03 12:31:10 +0000

    Create basic page

---
+++ __main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import flask
+import os
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("WEBPAGE_SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    return "Hello."
+
+app.run(port=int(os.environ.get("WEBPAGE_PORT")), debug=False)
