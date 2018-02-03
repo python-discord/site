003e46347bdabee10261ef504ff926fa97ee2034
JoeBanks13 <joseph@josephbanks.me>
2018-02-03 13:11:47 +0000

    add snekrc

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import flask
 import os
+
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -11,6 +12,7 @@ app.secret_key = os.environ.get("WEBPAGE_SECRET_KEY")
 @app.route("/")
 def index():
     return "Hello."
+
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("WEBPAGE_PORT")), debug=False)
