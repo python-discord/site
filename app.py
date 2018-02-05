beaa61362aed31f229c935a347e382024eee5a90
Gareth Coles <gdude2002@gmail.com>
2018-02-05 12:22:49 +0000

    Dynamic route loader; proper application structure
    Also fixed flake8-imports getting the other wrong

--- app.py
+++ app.py
@@ -1,35 +1,10 @@
-#!/usr/bin/env python3
+# coding=utf-8
 
-import os
-
-from flask import Flask
-from flask import jsonify
-from flask import redirect
-
-app = Flask(__name__)
-
-app.secret_key = os.environ.get("WEBPAGE_SECRET_KEY")
+from pysite.route_manager import RouteManager
 
 
-@app.route("/")
-def index():
-    return "Coming soon:tm:"
-
-
-@app.route("/invite")
-def invite():
-    return redirect("http://invite.pythondiscord.com/")
-
-
-@app.route("/healthcheck")
-def healthcheck():
-    return jsonify({"status": "ok"})
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "replace me with a template, 404 not found", 404
-
+manager = RouteManager()
+app = manager.app
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("WEBPAGE_PORT")), debug=False)
+    manager.run()
