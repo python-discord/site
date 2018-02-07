aaf007a9b07d81cd47dca157807c079ea4c561ea
Christopher Baklid <Inveracity@users.noreply.github.com>
2018-02-07 23:07:59 +0100

    Travis deploy on master (#5)
    * adds python script to handle branch condition

--- deploy.py
+++ deploy.py
@@ -7,9 +7,14 @@ branch = os.environ.get("TRAVIS_BRANCH")
 url = os.environ.get("AUTODEPLOY_WEBHOOK")
 token = os.environ.get("AUTODEPLOY_TOKEN")
 
-if branch == 'master':
+PR = os.environ.get("TRAVIS_PULL_REQUEST")
+
+print('branch:', branch)
+print('is_pr:', PR)
+
+if branch == 'master' and PR == 'false':
     print("deploying..")
-    result = requests.post(url=url, headers={'token': token})
+    result = requests.get(url=url, headers={'token': token})
     print(result.text)
 
 else:
