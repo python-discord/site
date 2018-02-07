66b1c3bea1f9f06104b78ae878a430f66f111fc3
Christopher Baklid <christopherb@unity3d.com>
2018-02-07 23:02:32 +0100

    check if PR

--- deploy.py
+++ deploy.py
@@ -6,10 +6,14 @@ import requests
 branch = os.environ.get("TRAVIS_BRANCH")
 url = os.environ.get("AUTODEPLOY_WEBHOOK")
 token = os.environ.get("AUTODEPLOY_TOKEN")
+PR = os.environ.get("TRAVIS_PULL_REQUEST")
 
-if branch == 'master':
+print('branch:', branch)
+print('is_pr:', PR)
+
+if branch == 'master' and PR == 'false':
     print("deploying..")
-    result = requests.post(url=url, headers={'token': token})
+    result = requests.get(url=url, headers={'token': token})
     print(result.text)
 
 else:
