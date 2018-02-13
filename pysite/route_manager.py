7acfa4c3d96a08527bb043866604ba517762c3ba
Jeremiah Boby <jeremiahboby@gmail.com>
2018-02-13 17:17:41 +0000

    Added a partners page
    Added the partners view, with a fontawesome navbar item and simple html linked banners.

--- pysite/route_manager.py
+++ pysite/route_manager.py
@@ -3,11 +3,11 @@ import importlib
 import inspect
 import os
 
-from flask import Flask, abort, g
+from flask import Blueprint, Flask, abort, g
 
 import rethinkdb
 
-from pysite.base_route import BaseView, ErrorView, RouteView
+from pysite.base_route import APIView, BaseView, ErrorView, RouteView
 
 DB_HOST = os.environ.get("RETHINKDB_HOST")
 DB_PORT = os.environ.get("RETHINKDB_PORT")
@@ -25,18 +25,30 @@ class RouteManager:
         )
         self.app.secret_key = os.environ.get("WEBPAGE_SECRET_KEY")
 
-        self.load_views()
+        self.main_blueprint = Blueprint("main", __name__)
+
+        print(f"Loading Blueprint: {self.main_blueprint.name}")
+        self.load_views(self.main_blueprint, "pysite/views/main")
+        self.app.register_blueprint(self.main_blueprint)
+        print("")
+
+        self.api_blueprint = Blueprint("api", __name__, subdomain="api")
+
+        print(f"Loading Blueprint: {self.api_blueprint.name}")
+        self.load_views(self.api_blueprint, "pysite/views/api")
+        self.app.register_blueprint(self.api_blueprint)
+        print("")
 
     def run(self):
         self.app.run(
             port=int(os.environ.get("WEBPAGE_PORT")), debug="FLASK_DEBUG" in os.environ
         )
 
-    def load_views(self, location="pysite/views"):
+    def load_views(self, blueprint, location="pysite/views"):
         for filename in os.listdir(location):
             if os.path.isdir(f"{location}/{filename}"):
                 # Recurse if it's a directory; load ALL the views!
-                self.load_views(location=f"{location}/{filename}")
+                self.load_views(blueprint, location=f"{location}/{filename}")
                 continue
 
             if filename.endswith(".py") and not filename.startswith("__init__"):
@@ -48,10 +60,11 @@ class RouteManager:
                             cls is not BaseView and
                             cls is not ErrorView and
                             cls is not RouteView and
+                            cls is not APIView and
                             BaseView in cls.__mro__
                     ):
-                        cls.setup(self.app)
-                        print(f"View loaded: {cls.name: <25} ({module.__name__}.{cls_name})")
+                        cls.setup(blueprint)
+                        print(f">> View loaded: {cls.name: <15} ({module.__name__}.{cls_name})")
 
     def setup_db(self):
         connection = self.get_db_connection(connect_database=False)
