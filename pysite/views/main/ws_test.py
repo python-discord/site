cf999735856504fee0bfe74d9b66e764b71ae746
Gareth Coles <gdude2002@gmail.com>
2018-02-13 10:45:57 +0000

    Websocket echo test

---
+++ pysite/views/main/ws_test.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import os
+
+from pysite.base_route import RouteView
+
+
+class WSTest(RouteView):
+    path = "/ws_test"
+    name = "ws_test"
+
+    def get(self):
+        return self.render(
+            "ws_test.html",
+            server_name=os.environ.get("SERVER_NAME", "localhost")
+        )
