6d5e2a3e0b6e04af6887231896cd33cdb949b713
Gareth Coles <gdude2002@gmail.com>
2018-02-15 15:22:46 +0000

    Add some constants for our roles
    These will be important later

--- pysite/constants.py
+++ pysite/constants.py
@@ -8,3 +8,9 @@ class ErrorCodes(IntEnum):
     unauthorized = 1
     invalid_api_key = 2
     missing_parameters = 3
+
+
+OWNER_ROLE = 267627879762755584
+ADMIN_ROLE = 267628507062992896
+MODERATOR_ROLE = 267629731250176001
+HELPER_ROLE = 267630620367257601
