09c5972f7d8bafdcea72337ecb3ab588550b0553
Leon Sandøy <leon.sandoy@conmodo.com>
2018-02-27 10:06:16 +0100

    renaming tag.py to tags.py and refactoring the nomenclature of docs to tags

--- pysite/views/api/bot/tags.py
+++ pysite/views/api/bot/tags.py
@@ -8,44 +8,44 @@ from pysite.decorators import api_key
 from pysite.mixins import DBMixin
 
 
-class DocsView(APIView, DBMixin):
-    path = "/docs"
-    name = "docs"
-    table_name = "docs"
-    table_primary_key = "doc_name"
+class TagsView(APIView, DBMixin):
+    path = "/tags"
+    name = "tags"
+    table_name = "tags"
+    table_primary_key = "tag_name"
 
     @api_key
     def get(self):
         """
-        Fetches docs from the database.
-        - If doc_name is provided, it fetches
-        that specific doc.
-        - If doc_category is provided, it fetches
-        all docs in that category.
+        Fetches tags from the database.
+        - If tag_name is provided, it fetches
+        that specific tag.
+        - If tag_category is provided, it fetches
+        all tags in that category.
         - If nothing is provided, it will
-        fetch a list of all doc_names.
+        fetch a list of all tag_names.
 
         Data must be provided as params.
         API key must be provided as header.
         """
 
-        doc_name = request.args.get("doc_name")
+        tag_name = request.args.get("tag_name")
 
-        if doc_name:
-            data = self.db.get(self.table_name, doc_name) or {}
+        if tag_name:
+            data = self.db.get(self.table_name, tag_name) or {}
         else:
-            data = self.db.pluck(self.table_name, "doc_name") or []
+            data = self.db.pluck(self.table_name, "tag_name") or []
 
         return jsonify(data)
 
     @api_key
     def post(self):
         """
-        If the doc_name doesn't exist, this
-        saves a new doc in the database.
+        If the tag_name doesn't exist, this
+        saves a new tag in the database.
 
-        If the doc_name already exists,
-        this will edit the existing doc.
+        If the tag_name already exists,
+        this will edit the existing tag.
 
         Data must be provided as JSON.
         API key must be provided as header.
@@ -54,15 +54,15 @@ class DocsView(APIView, DBMixin):
 
         data = request.get_json()
 
-        doc_name = data.get("doc_name")
-        doc_content = data.get("doc_content")
+        tag_name = data.get("tag_name")
+        tag_content = data.get("tag_content")
 
-        if doc_name and doc_content:
+        if tag_name and tag_content:
             self.db.insert(
                 self.table_name,
                 {
-                    "doc_name": doc_name,
-                    "doc_content": doc_content
+                    "tag_name": tag_name,
+                    "tag_content": tag_content
                 },
                 conflict="update"  # if it exists, update it.
             )
@@ -74,19 +74,19 @@ class DocsView(APIView, DBMixin):
     @api_key
     def delete(self):
         """
-        Deletes a doc from the database.
+        Deletes a tag from the database.
 
         Data must be provided as JSON.
         API key must be provided as header.
         """
 
         data = request.get_json()
-        doc_name = data.get("doc_name")
+        tag_name = data.get("tag_name")
 
-        if doc_name:
+        if tag_name:
             changes = self.db.delete(
                 self.table_name,
-                doc_name,
+                tag_name,
                 return_changes=True
             )
 
