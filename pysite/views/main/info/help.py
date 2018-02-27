6aaddfd14beaafed9a55a2c0074fbaad4dda0f50
Gareth Coles <gdude2002@gmail.com>
2018-02-27 09:44:19 +0000

    Move `/help` to `/info/help`

--- pysite/views/main/info/help.py
+++ pysite/views/main/info/help.py
@@ -3,8 +3,8 @@ from pysite.base_route import RouteView
 
 
 class HelpView(RouteView):
-    path = "/help"
-    name = "help"
+    path = "/info/help"
+    name = "info/help"
 
     def get(self):
-        return self.render("main/help.html")
+        return self.render("main/info/help.html")
