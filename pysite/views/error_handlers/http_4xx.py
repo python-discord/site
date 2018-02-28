2823ea59266abe4ca50aa99c72427d2748efa8b9
JoeBanks13 <joseph@josephbanks.me>
2018-02-28 22:48:33 +0000

    Enable REPL on 4XX and change error descriptions

---
+++ pysite/views/error_handlers/http_4xx.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from flask import render_template, request
+from werkzeug.exceptions import NotFound
+
+from pysite.base_route import ErrorView
+from pysite.constants import ERROR_DESCRIPTIONS
+
+
+class Error400View(ErrorView):
+    name = "error_4xx"
+    error_code = range(400, 430)
+
+    def get(self, error: NotFound):
+        error_desc = ERROR_DESCRIPTIONS.get(error.code, "We're not really sure what happened there, please try again.")
+
+        return render_template("errors/error.html", code=error.code, req=request, error_title=error_desc,
+                               error_message=error_desc +
+                               " If you believe we have made a mistake "
+                               "please open an issue "
+                               "on our GitHub ("
+                               "https://github.com"
+                               "/discord-python/site/issues)."), error.code
--- pysite/views/error_handlers/http_5xx.py
+++ pysite/views/error_handlers/http_5xx.py
@@ -3,6 +3,7 @@ from flask import render_template, request
 from werkzeug.exceptions import HTTPException
 
 from pysite.base_route import ErrorView
+from pysite.constants import ERROR_DESCRIPTIONS
 
 
 class Error500View(ErrorView):
@@ -10,4 +11,15 @@ class Error500View(ErrorView):
     error_code = range(500, 600)
 
     def get(self, error: HTTPException):
-        return render_template("errors/5XX.html", code=error.code, req=request), error.code
+        error_desc = ERROR_DESCRIPTIONS.get(error.code, "We're not really sure what happened there, please try again.")
+
+        return render_template("errors/error.html", code=error.code, req=request, error_title=error_desc,
+                               error_message="An error occured while "
+                                             "processing this "
+                                             "request, please try "
+                                             "again later. "
+                                             "If you believe we have made a mistake "
+                                             "please open an issue "
+                                             "on our GitHub ("
+                                             "https://github.com"
+                                             "/discord-python/site/issues)."), error.code
