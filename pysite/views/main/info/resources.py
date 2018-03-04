8ec05dadc4b446969149d440f8b9b741d3494f27
Gareth Coles <gdude2002@gmail.com>
2018-03-04 12:25:41 +0000

    Basic resources page setup

--- pysite/views/main/info/resources.py
+++ pysite/views/main/info/resources.py
@@ -1,5 +1,16 @@
 # coding=utf-8
+import json
+
+from logging import getLogger
 from pysite.base_route import RouteView
+
+
+try:
+    with open("static/resources.json") as fh:
+        categories = json.load(fh)
+except Exception:
+    getLogger("Resources").exception("Failed to load resources.json")
+    categories = None
 
 
 class ResourcesView(RouteView):
@@ -7,4 +18,4 @@ class ResourcesView(RouteView):
     name = "info/resources"
 
     def get(self):
-        return self.render("main/info/resources.html")
+        return self.render("main/info/resources.html", categories=categories)
