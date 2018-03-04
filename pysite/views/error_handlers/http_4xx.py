e7ac41f1cb94e759bd9072b92d8adc62259db5a2
Gareth Coles <gdude2002@gmail.com>
2018-03-04 11:43:40 +0000

    Fix error views not using `self.render()`

--- pysite/views/error_handlers/http_4xx.py
+++ pysite/views/error_handlers/http_4xx.py
@@ -13,17 +13,17 @@ class Error400View(ErrorView):
     def get(self, error: HTTPException):
         error_desc = ERROR_DESCRIPTIONS.get(error.code, "We're not really sure what happened there, please try again.")
 
-        return render_template("errors/error.html", code=error.code, req=request, error_title=error_desc,
-                               error_message=error_desc +
-                               " If you believe we have made a mistake, please "
-                               "<a href='https://github.com"
-                               "/discord-python/site/issues'>open an issue on our GitHub</a>."), error.code
+        return self.render("errors/error.html", code=error.code, req=request, error_title=error_desc,
+                           error_message=error_desc +
+                           " If you believe we have made a mistake, please "
+                           "<a href='https://github.com"
+                           "/discord-python/site/issues'>open an issue on our GitHub</a>."), error.code
 
     def post(self, error: HTTPException):
         error_desc = ERROR_DESCRIPTIONS.get(error.code, "We're not really sure what happened there, please try again.")
 
-        return render_template("errors/error.html", code=error.code, req=request, error_title=error_desc,
-                               error_message=error_desc +
-                               " If you believe we have made a mistake, please "
-                               "<a href='https://github.com"
-                               "/discord-python/site/issues'>open an issue on our GitHub</a>."), error.code
+        return self.render("errors/error.html", code=error.code, req=request, error_title=error_desc,
+                           error_message=error_desc +
+                           " If you believe we have made a mistake, please "
+                           "<a href='https://github.com"
+                           "/discord-python/site/issues'>open an issue on our GitHub</a>."), error.code
