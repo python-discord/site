751812ac54b561d6ae44927c950e927c5070818d
Christopher Baklid <christopherb@unity3d.com>
2018-03-10 15:18:31 +0100

    adds docker-compose file

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 # coding=utf-8
 
+from os import environ
+
 from pysite.route_manager import RouteManager
 
 manager = RouteManager()
 app = manager.app
+
+debug = environ.get('DEBUG', "0")
+if debug == "1":
+    print('good times')
+    app.jinja_env.auto_reload = True
 
 if __name__ == '__main__':
     manager.run()
