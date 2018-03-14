1aaa8e92237de6b477f3e132d89f54b8496e17eb
Gareth Coles <gdude2002@gmail.com>
2018-03-14 12:07:12 +0000

    Early bersion of the code jams page

---
+++ pysite/views/main/info/jams.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from pysite.base_route import RouteView
+
+
+class IndexView(RouteView):
+    path = "/info/jams"
+    name = "info/jams"
+
+    def get(self):
+        return self.render("main/info/jams.html")
