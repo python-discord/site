00ba86847037838ae01d4b509c8a3a60cf6f3d8c
Gareth Coles <gdude2002@gmail.com>
2018-04-05 10:02:03 +0100

    RST test error catching

--- pysite/views/ws/rst.py
+++ pysite/views/ws/rst.py
@@ -21,11 +21,16 @@ class RSTWebsocket(WS):
 
     def on_message(self, message):
         self.log.debug(f"RST | Message: {message}")
-        self.send(
-            publish_parts(
+
+        try:
+            data = publish_parts(
                 source=message, writer_name="html5", settings_overrides={"traceback": True}
             )["html_body"]
-        )
+        except Exception as e:
+            self.log.exception("Parsing error")
+            data = str(e)
+
+        self.send(data)
 
     def on_close(self):
         self.log.debug("RST | WS closed.")
