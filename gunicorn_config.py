c43b15ce0727842e1388c0b6facefc3f7e73839c
Gareth Coles <gdude2002@gmail.com>
2018-04-07 10:43:35 +0100

    Fix logging in gunicorn_config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,12 +1,12 @@
 # coding=utf-8
-def when_ready(_server):
+def when_ready(server):
     """ server hook that only runs when the gunicorn master process loads """
 
-    print("Creating tables...")
+    server.log.info("Creating tables...")
 
     from pysite.database import RethinkDB
 
     db = RethinkDB(loop_type=None)
     created = db.create_tables()
 
-    print(f"Created {created} tables.")
+    server.log.info(f"Created {created} tables.")
