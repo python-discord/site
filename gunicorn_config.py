886fb0b06117e56cc70767424b718c90e2789ef4
Gareth Coles <gdude2002@gmail.com>
2018-04-08 01:13:26 +0100

    Fix build

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,12 +1,21 @@
 # coding=utf-8
-def when_ready(server):
+def when_ready(server=None):
     """ server hook that only runs when the gunicorn master process loads """
 
-    server.log.info("Creating tables...")
+    if server:
+        output = server.log.info
+    else:
+        output = print
+
+    output("Creating tables...")
 
     from pysite.database import RethinkDB
 
     db = RethinkDB(loop_type=None)
     created = db.create_tables()
 
-    server.log.info(f"Created {created} tables.")
+    output(f"Created {created} tables.")
+
+
+if __name__ == "__main__":
+    when_ready()
