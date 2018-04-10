47887fbb33c306093647975cfa72010485333757
Gareth Coles <gdude2002@gmail.com>
2018-04-10 09:53:54 +0100

    [Wiki] Add code highlighting

--- pysite/rst/__init__.py
+++ pysite/rst/__init__.py
@@ -7,7 +7,7 @@ from pysite.rst.roles import icon_role
 
 def render(rst: str):
     return publish_parts(
-        source=rst, writer_name="html5", settings_overrides={"halt_level": 2}
+        source=rst, writer_name="html5", settings_overrides={"halt_level": 2, "syntax_highlight": "short"}
     )["html_body"]
 
 
