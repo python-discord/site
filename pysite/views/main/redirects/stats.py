00e6c0f1540b9f0c6a57e2b10fc6d17a22bf6d11
Leon Sandøy <leon.sandoy@conmodo.com>
2018-04-10 22:45:23 +0200

    Moved all redirects into a folder and added one for github.

--- pysite/views/main/redirects/stats.py
+++ pysite/views/main/redirects/stats.py
@@ -4,9 +4,9 @@ from flask import redirect
 from pysite.base_route import RouteView
 
 
-class DatadogView(RouteView):
-    path = "/datadog"
-    name = "datadog"
+class StatsView(RouteView):
+    path = "/stats"
+    name = "stats"
 
     def get(self):
         return redirect("https://p.datadoghq.com/sb/ac8680a8c-c01b556f01b96622fd4f57545b81d568")
