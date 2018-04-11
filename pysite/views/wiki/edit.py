618cd23e92dcb2ac0653023b5e459248270a09c3
JoeBanks13 <joseph@josephbanks.me>
2018-04-11 23:21:01 +0100

    Add wiki audit logs

--- pysite/views/wiki/edit.py
+++ pysite/views/wiki/edit.py
@@ -1,9 +1,12 @@
 # coding=utf-8
+import difflib
+import datetime
+import requests
 from flask import request, url_for
 from werkzeug.utils import redirect
 
 from pysite.base_route import RouteView
-from pysite.constants import EDITOR_ROLES
+from pysite.constants import EDITOR_ROLES, WIKI_AUDIT_WEBHOOK, GITHUB_TOKEN
 from pysite.decorators import csrf, require_roles
 from pysite.mixins import DBMixin
 from pysite.rst import render
@@ -34,6 +37,14 @@ class EditView(RouteView, DBMixin):
     @require_roles(*EDITOR_ROLES)
     @csrf
     def post(self, page):
+        before = self.db.get(self.table_name, page)
+        if not before:
+            before = []
+        else:
+            before = before["rst"].splitlines(keepends=True)
+            if not before[-1].endswith("\n"):
+                before[-1] += "\n" # difflib makes the output look weird if there isn't a newline on the last line
+
         rst = request.form["rst"]
         obj = {
             "slug": page,
@@ -47,5 +58,42 @@ class EditView(RouteView, DBMixin):
             obj,
             conflict="replace"
         )
+
+        after = obj['rst'].splitlines(keepends=True) or []
+        if not after[-1].endswith("\n"):
+            after[-1] += "\n" # Does same thing as L41
+
+        diff = difflib.unified_diff(before, after, fromfile="before.rst", tofile="after.rst")
+        diff = "".join(diff)
+
+        gist_payload = {
+            "description": f"Changes to: {obj['title']}",
+            "public": False,
+            "files": {
+                "changes.md": {
+                    "content": f"```diff\n{diff}\n```"
+                }
+            }
+        }
+
+        gist = requests.post("https://api.github.com/gists", json=gist_payload, headers={"Authorization": f"token {GITHUB_TOKEN}"})
+        print(gist.text)
+
+        audit_payload = {
+            "username": "Wiki Updates",
+            "embeds": [
+                {
+                    "title": "Page Edit",
+                    "description": f"**{obj['title']}** was edited by **Joseph**.\n\n[View diff]({gist.json().get('html_url')})",
+                    "color": 4165079,
+                    "timestamp": datetime.datetime.utcnow().isoformat(),
+                    "thumbnail": {
+                        "url": "https://pythondiscord.com/static/logos/logo_discord.png"
+                    }
+                }
+            ]
+        }
+
+        print(requests.post(WIKI_AUDIT_WEBHOOK, json=audit_payload).text)
 
         return redirect(url_for("wiki.page", page=page), code=303)  # Redirect, ensuring a GET
