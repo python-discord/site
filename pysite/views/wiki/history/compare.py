451bc5c53728e71bdd90892c45fd467d65c9b74a
JoeBanks13 <joseph@josephbanks.me>
2018-04-12 22:10:04 +0100

    Add compare route to render diff

---
+++ pysite/views/wiki/history/compare.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+import difflib
+
+from pygments import highlight
+from pygments.lexers import DiffLexer
+from pygments.formatters import HtmlFormatter
+from flask import abort
+
+from pysite.base_route import RouteView
+from pysite.mixins import DBMixin
+
+
+class CompareView(RouteView, DBMixin):
+    path = "/history/compare/<string:first_rev>/<string:second_rev>"
+    name = "history.compare"
+
+    table_name = "revisions"
+    table_primary_key = "id"
+
+    def get(self, first_rev, second_rev):
+        before = self.db.get(self.table_name, first_rev)
+        after = self.db.get(self.table_name, second_rev)
+
+        if not before or not after:
+            abort(404)
+
+        before_text = before["post"]["rst"]
+        after_text = after["post"]["rst"]
+
+        if not before_text.endswith("\n"):
+            before_text += "\n"
+
+        if not after_text.endswith("\n"):
+            after_text += "\n"
+
+        before_text = before_text.splitlines(keepends=True)
+        after_text = after_text.splitlines(keepends=True)
+
+        if not before["slug"] == after["slug"]:
+            abort(400)  # The revisions are not from the same post
+
+        diff = difflib.unified_diff(before_text, after_text, fromfile=f"{first_rev}.rst", tofile=f"{second_rev}.rst")
+        diff = "".join(diff)
+        diff = highlight(diff, DiffLexer(), HtmlFormatter())
+        return self.render("wiki/compare_revision.html", title=after["post"]["title"], diff=diff)
---
+++ pysite/views/wiki/history/show.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import datetime
+from flask import abort
+
+from pysite.base_route import RouteView
+from pysite.mixins import DBMixin
+
+
+class RevisionsListView(RouteView, DBMixin):
+    path = "/history/show/<string:page>"
+    name = "error"
+
+    table_name = "revisions"
+    table_primary_key = "id"
+
+    def get(self, page):
+        results = self.db.filter(self.table_name, lambda revision: revision["slug"] == page)
+        if len(results) == 0:
+            abort(404)
+
+        for result in results:
+            ts = datetime.datetime.fromtimestamp(result["date"])
+            result["pretty_time"] = ts.strftime("%d %b %Y")
+
+        results = sorted(results, key=lambda revision: revision["date"])
+        return self.render("wiki/revision_list.html", page=page, revisions=results), 200
