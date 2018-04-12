ac72aa5b5f69e1fe86f8b787bc108e30a197fe74
JoeBanks13 <joseph@josephbanks.me>
2018-04-12 22:11:08 +0100

    Add revision list route

--- pysite/views/wiki/history/show.py
+++ pysite/views/wiki/history/show.py
@@ -8,7 +8,7 @@ from pysite.mixins import DBMixin
 
 class RevisionsListView(RouteView, DBMixin):
     path = "/history/show/<string:page>"
-    name = "error"
+    name = "history.show"
 
     table_name = "revisions"
     table_primary_key = "id"
@@ -22,5 +22,5 @@ class RevisionsListView(RouteView, DBMixin):
             ts = datetime.datetime.fromtimestamp(result["date"])
             result["pretty_time"] = ts.strftime("%d %b %Y")
 
-        results = sorted(results, key=lambda revision: revision["date"])
+        results = sorted(results, key=lambda revision: revision["date"], reverse=True)
         return self.render("wiki/revision_list.html", page=page, revisions=results), 200
