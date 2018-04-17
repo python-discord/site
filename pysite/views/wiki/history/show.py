16c86d153fd47093e384c049081cc7ef051032d9
Joseph <joseph@josephbanks.me>
2018-04-18 00:21:33 +0100

    Pass can_edit into revisions list

--- pysite/views/wiki/history/show.py
+++ pysite/views/wiki/history/show.py
@@ -25,7 +25,7 @@ class RevisionsListView(RouteView, DBMixin):
             result["pretty_time"] = ts.strftime("%d %b %Y")
 
         results = sorted(results, key=lambda revision: revision["date"], reverse=True)
-        return self.render("wiki/revision_list.html", page=page, revisions=results), 200
+        return self.render("wiki/revision_list.html", page=page, revisions=results, can_edit=self.is_staff()), 200
 
     def is_staff(self):
         if DEBUG_MODE:
