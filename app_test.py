f4784beadf37392efee3606a92a404eb39e9925f
Johannes Christ <Johannes.christ@gmail.com>
2018-04-24 16:55:59 +0200

    Add tests for the `doc` API.

--- app_test.py
+++ app_test.py
@@ -234,6 +234,74 @@ class ApiEndpoints(SiteTest):
         response = self.client.post('/user', app.config['API_SUBDOMAIN'], headers=headers, data=list_data)
         self.assertTrue("inserted" in response.json)
 
+    def test_api_docs(self):
+        """ Check documentation metadata API """
+        os.environ['BOT_API_KEY'] = 'abcdefg'
+        headers = {'X-API-Key': 'abcdefg', 'Content-Type': 'application/json'}
+        bad_data = json.dumps({'package': 'whatever', 'correct format': False})
+        valid_data = {
+            'package': "lemonapi",
+            'base_url': "http://example.com/",
+            'inventory_url': "http://example.com/object.inv"
+        }
+        valid_data_json = json.dumps(valid_data)
+        unknown_package_json = json.dumps({'package': "whatever"})
+        delete_data_json = json.dumps({'package': valid_data['package']})
+
+        # GET - all entries
+        response = self.client.get('/docs', app.config['API_SUBDOMAIN'], headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, list)
+
+        # GET - unknown package
+        response = self.client.get('/docs?package=whatever', app.config['API_SUBDOMAIN'], headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {})
+
+        # POST - no data
+        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=headers)
+        self.assertEqual(response.status_code, 400)
+
+        # POST - malformed data
+        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=headers, data=bad_data)
+        self.assertEqual(response.status_code, 400)
+
+        # POST - valid data
+        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=headers, data=valid_data_json)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"success": True})
+
+        # GET - added package in all entries
+        response = self.client.get('/docs', app.config['API_SUBDOMAIN'], headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(valid_data, response.json)
+
+        # GET - added package detail
+        response = self.client.get(
+            f'/docs?package={valid_data["package"]}', app.config['API_SUBDOMAIN'], headers=headers
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, valid_data)
+
+        # DELETE - missing request body
+        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=headers)
+        self.assertEqual(response.status_code, 400)
+
+        # DELETE - unknown package
+        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=headers, data=unknown_package_json)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"success": False})
+
+        # DELETE - added package
+        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=headers, data=delete_data_json)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"success": True})
+
+        # GET - added package is no longer in all entries
+        response = self.client.get('/docs', app.config['API_SUBDOMAIN'], headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(valid_data, response.json)
+
     def test_api_route_errors(self):
         """ Check api route errors """
         from pysite.base_route import APIView
