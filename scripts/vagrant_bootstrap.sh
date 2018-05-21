4d97fbf02d6fc2aedde1d5000be1675931837a13
Christopher Baklid <christopherb@unity3d.com>
2018-05-21 21:00:02 +0200

    update vagrant script [ci skip]

--- scripts/vagrant_bootstrap.sh
+++ scripts/vagrant_bootstrap.sh
@@ -1,87 +1,68 @@
 #!/bin/bash
 
 # Dependencies
-apt-get update
-apt-get install -y software-properties-common
-apt-get install -y python-software-properties
-apt-get install -y curl
-apt-get install -y apt-transport-https
+apt install -y software-properties-common
+apt install -y python-software-properties
+apt install -y curl
+
+# Python 3.6
 add-apt-repository -y ppa:jonathonf/python-3.6
-echo "deb http://download.rethinkdb.com/apt xenial main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
-apt-get update
 
-# Python3.6
-apt-get install -y python3.6
-apt-get install -y python3.6-dev
-apt-get install -y build-essential
+# Rethinkdb
+curl -fsSL https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+echo "deb http://download.rethinkdb.com/apt xenial main" | tee /etc/apt/sources.list.d/rethinkdb.list
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install
+apt update
+apt install -y apt-transport-https
+apt install -y docker-ce
+apt install -y rethinkdb
+apt install -y python3.6
+apt install -y python3.6-dev
+apt install -y build-essential
+apt install -y python3-distutils
+
+# Compose
+curl -Ls https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Pip
+curl -s https://bootstrap.pypa.io/get-pip.py | python -
+curl -s https://bootstrap.pypa.io/get-pip.py | python3 -
 curl -s https://bootstrap.pypa.io/get-pip.py | python3.6 -
-python3.6 -m pip install -r /vagrant/requirements.txt
-python3.6 -m pip install -r /vagrant/requirements-ci.txt
-python3.6 -m pip install gunicorn
 
-# RethinkDB
-apt-get install -y rethinkdb
+# Pipenv
+python3.6 -m pip install pipenv
 
+# RethinkDB config
 tee /etc/rethinkdb/instances.d/rethinkdb.conf <<EOF
 runuser=root
 rungroup=root
 bind=0.0.0.0
-driver-port=28016
+driver-port=28015
 http-port=28010
 EOF
 
 service rethinkdb restart
 
-# development environment variables
-tee /root/.bashrc <<EOF
-HISTCONTROL=ignoreboth
-shopt -s histappend
-HISTSIZE=1000
-HISTFILESIZE=2000
-shopt -s checkwinsize
-
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-case "$TERM" in
-    xterm-color|*-256color) color_prompt=yes;;
-esac
-
-PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-
-test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias ll='ls -alF --color=auto'
-alias la='ls -A --color=auto'
-alias l='ls -CF --color=auto'
-
-export LOG_LEVEL=DEBUG
-export SERVER_NAME="pysite.local"
-export WEBPAGE_PORT="80"
-export WEBPAGE_SECRET_KEY="123456789abcdefghijklmn"
-export RETHINKDB_HOST="127.0.0.1"
-export RETHINKDB_PORT="28016"
-export RETHINKDB_DATABASE="database"
-export RETHINKDB_TABLE="table"
-export BOT_API_KEY="abcdefghijklmnopqrstuvwxyz"
-export TEMPLATES_AUTO_RELOAD="yes"
-export PREFERRED_URL_SCHEME="http"
-alias python=python3.6
-EOF
-
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce
-
-curl -L https://github.com/docker/compose/releases/download/1.20.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-
-echo 'docs: https://github.com/discord-python/site/wiki/Development-Environment-(Vagrant)'
+# Environment variables
+cat > /etc/environment <<EOL
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+VIRTUALENV_ALWAYS_COPY=1
+PIPENV_VENV_IN_PROJECT=1
+PIPENV_IGNORE_VIRTUALENVS=1
+SERVER_NAME="pythondiscord.local"
+WEBPAGE_PORT="80"
+WEBPAGE_SECRET_KEY="123456789abcdefghijklmn"
+RETHINKDB_HOST="127.0.0.1"
+RETHINKDB_PORT="28015"
+RETHINKDB_DATABASE="pythondiscord"
+BOT_API_KEY="abcdefghijklmnopqrstuvwxyz"
+TEMPLATES_AUTO_RELOAD="yes"
+PREFERRED_URL_SCHEME="http"
+PYTHONPATH="/vagrant/pysite"
+EOL
