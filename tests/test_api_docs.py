3ff9ee97100e038897b7440eb2fde9f45cbb2841
Christopher Baklid <christopherb@unity3d.com>
2018-05-21 09:24:44 +0200

    make each docs test a function for greater test report visibility

--- tests/test_api_docs.py
+++ tests/test_api_docs.py
@@ -1,82 +1,107 @@
-import os
 import json
+import pytest
 from tests import SiteTest, app
 
-class ApiBotUsersEndpoint(SiteTest):
-    def test_api_docs(self):
-        """ Check documentation metadata API """
-        bad_data = json.dumps({'package': 'whatever', 'correct format': False})
-        valid_data = {
-            'package': "lemonapi",
-            'base_url': "http://example.com/",
-            'inventory_url': "http://example.com/object.inv"
-        }
-        valid_data_json = json.dumps(valid_data)
-        unknown_package_json = json.dumps({'package': "whatever"})
-        delete_data_json = json.dumps({'package': valid_data['package']})
+class ApiDocsEndpoint(SiteTest):
+    """ Check documentation metadata API """
+    bad_data = json.dumps({'package': 'whatever', 'correct format': False})
+    valid_data = {
+        'package': "lemonapi",
+        'base_url': "http://example.com/",
+        'inventory_url': "http://example.com/object.inv"
+    }
+    valid_data_json = json.dumps(valid_data)
+    unknown_package_json = json.dumps({'package': "whatever"})
+    delete_data_json = json.dumps({'package': valid_data['package']})
 
-        # GET - all entries
+    @pytest.mark.run(order=1)
+    def test_api_docs_get_all(self):
+        """ GET - all entries """
         response = self.client.get('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json, list)
 
-        # GET - unknown package
+    @pytest.mark.run(order=2)
+    def test_api_docs_get_unknown(self):
+        """ GET - unknown package """
         response = self.client.get('/docs?package=whatever', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, [])
 
-        # GET - multiple unknown packages
+    @pytest.mark.run(order=3)
+    def test_api_docs_get_multi_unknown(self):
+        """ GET - multiple unknown packages """
         response = self.client.get('/docs?package=whatever&package=everwhat', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json, list)
 
-        # POST - no data
+    @pytest.mark.run(order=4)
+    def test_api_docs_post_no_data(self):
+        """ POST - no data """
         response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 400)
 
-        # POST - malformed data
-        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=bad_data)
+    @pytest.mark.run(order=5)
+    def test_api_docs_post_bad_data(self):
+        """ POST - malformed data """
+        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=self.bad_data)
         self.assertEqual(response.status_code, 400)
 
-        # POST - valid data
-        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=valid_data_json)
+    @pytest.mark.run(order=6)
+    def test_api_docs_post_good_data(self):
+        """ POST - valid data """
+        response = self.client.post('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=self.valid_data_json)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"success": True})
 
-        # GET - added package in all entries
+    @pytest.mark.run(order=7)
+    def test_api_docs_get_valid(self):
+        """ GET - added package in all entries """
         response = self.client.get('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 200)
-        self.assertIn(valid_data, response.json)
+        self.assertIn(self.valid_data, response.json)
 
-        # GET - added package detail
+    @pytest.mark.run(order=8)
+    def test_api_docs_get_detail(self):
+        """ GET - added package detail """
         response = self.client.get(
-            f'/docs?package={valid_data["package"]}', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER']
+            f'/docs?package={self.valid_data["package"]}', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER']
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, [valid_data])
+        self.assertEqual(response.json, [self.valid_data])
 
-        # GET - added package is the only package for query with another unknown package
+    @pytest.mark.run(order=9)
+    def test_api_docs_get_partly_known(self):
+        """ GET - added package is the only package for query with another unknown package """
         response = self.client.get(
-            f'/docs?package={valid_data["package"]}&package=whatever', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER']
+            f'/docs?package={self.valid_data["package"]}&package=whatever', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER']
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, [valid_data])
+        self.assertEqual(response.json, [self.valid_data])
 
-        # DELETE - missing request body
+    @pytest.mark.run(order=10)
+    def test_api_docs_delete_bad(self):
+        """ DELETE - missing request body """
         response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 400)
 
-        # DELETE - unknown package
-        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=unknown_package_json)
+    @pytest.mark.run(order=11)
+    def test_api_docs_delete_unknown(self):
+        """ DELETE - unknown package """
+        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=self.unknown_package_json)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['deleted'], 0)
 
-        # DELETE - added package
-        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=delete_data_json)
+    @pytest.mark.run(order=12)
+    def test_api_docs_delete_all(self):
+        """ DELETE - added package """
+        response = self.client.delete('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'], data=self.delete_data_json)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['deleted'], 1)
 
-        # GET - added package is no longer in all entries
+    @pytest.mark.run(order=13)
+    def test_api_docs_get_missing(self):
+        """ GET - added package is no longer in all entries """
         response = self.client.get('/docs', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn(valid_data, response.json)
+        self.assertNotIn(self.valid_data, response.json)
