e4c5fb8061b7b952622b05a929255855a2efe5d2
Gareth Coles <gdude2002@gmail.com>
2018-06-07 22:05:34 +0100

    Fix test

--- tests/test_api_bot_users.py
+++ tests/test_api_bot_users.py
@@ -5,8 +5,12 @@ from tests import SiteTest, app
 class ApiBotUsersEndpoint(SiteTest):
     def test_api_user(self):
         """ Check insert user """
-        single_data = json.dumps({'user_id': "1234", 'roles': ["5678"], "username": "test", "discriminator": "0000"})
-        list_data = json.dumps([{'user_id': "1234", 'roles': ["5678"], "username": "test", "discriminator": "0000"}])
+        single_data = json.dumps(
+            {'user_id': "1234", 'roles': ["5678"], "username": "test", "discriminator": "0000", "avatar": "http://some/url"}
+        )
+        list_data = json.dumps([
+            {'user_id': "1234", 'roles': ["5678"], "username": "test", "discriminator": "0000", "avatar": "http://some/url"}
+        ])
 
         response = self.client.get('/bot/users', app.config['API_SUBDOMAIN'], headers=app.config['TEST_HEADER'])
         self.assertEqual(response.status_code, 405)
