52a10c355eac2b50843a7b5a36557eced512b294
Gareth Coles <gdude2002@gmail.com>
2018-06-12 14:08:17 +0100

    It's always the smallest changes that don't lint

--- pysite/views/staff/jams/actions.py
+++ pysite/views/staff/jams/actions.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 
 from pysite.base_route import APIView
-from pysite.constants import ALL_STAFF_ROLES, BotEventTypes, ErrorCodes, JAMMERS_ROLE, CHANNEL_JAM_LOGS
+from pysite.constants import ALL_STAFF_ROLES, BotEventTypes, CHANNEL_JAM_LOGS, ErrorCodes, JAMMERS_ROLE
 from pysite.decorators import csrf, require_roles
 from pysite.mixins import DBMixin, RMQMixin
 
