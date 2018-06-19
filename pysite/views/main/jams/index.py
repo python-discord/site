ca900f7a8e363dbc832cb77149a980adcd225102
momothereal <momothereal.mc@gmail.com>
2018-06-19 14:53:49 -0400

    Disable the join button if the user has already applied to a code jam

--- pysite/views/main/jams/index.py
+++ pysite/views/main/jams/index.py
@@ -17,5 +17,19 @@ class JamsIndexView(RouteView, DBMixin):
             .limit(5)
         )
 
+        def has_applied_to_jam(jam):
+            # whether the user has applied to this jam
+            if not self.logged_in:
+                return False
+            return self.get_jam_response(jam, self.user_data["user_id"])
+
         jams = self.db.run(query, coerce=list)
-        return self.render("main/jams/index.html", jams=jams)
+        return self.render("main/jams/index.html", jams=jams, has_applied_to_jam=has_applied_to_jam)
+
+    def get_jam_response(self, jam, user_id):
+        query = self.db.query("code_jam_responses").filter({"jam": jam, "snowflake": user_id})
+        result = self.db.run(query, coerce=list)
+
+        if result:
+            return result[0]
+        return None
