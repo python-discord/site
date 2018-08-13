f72e333dc49f99436136b9c1a0a6287ceebb76b2
Johannes Christ <jc@jchri.st>
2018-08-13 23:35:26 +0200

    Namespace `home:index` URL.

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
 
 app_name = 'home'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/index.html'))
+    path('', TemplateView.as_view(template_name='home/index.html'), name='index')
 ]
