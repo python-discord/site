da1820ae69188c3e390ea511da522b0534819793
Johannes Christ <jc@jchri.st>
2018-08-13 23:36:14 +0200

    Add a simple test case for `home:index`.

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,9 @@
+from django.shortcuts import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestIndexReturns200(TestCase):
+    def test_index_returns_200(self):
+        url = reverse('home:index')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
