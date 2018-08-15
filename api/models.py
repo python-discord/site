c5950d3e0782a8553e6e2f070618b58fc6a3677f
Johannes Christ <jc@jchri.st>
2018-08-15 19:29:09 +0200

    Add the `SnakeName` model.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class SnakeName(models.Model):
+    name = models.CharField(primary_key=True, max_length=100)
+    scientific = models.CharField(max_length=150)
