6b5be23b4e7a023a4bef85dbe1359e7b5e1e4986
Johannes Christ <jc@jchri.st>
2018-08-16 19:30:00 +0200

    Add tests for the snake name listing API.

--- api/tests/test_snake_names.py
+++ api/tests/test_snake_names.py
@@ -1,10 +1,47 @@
 from django_hosts.resolvers import reverse
-from rest_framework.test import APITestCase, force_authenticate
+
+from .base import APISubdomainTestCase
+from ..models import SnakeName
 
 
-class EmptyDatabaseSnakeNameTests(APITestCase):
-    def test_endpoint_returns_empty_list(self):
-        import pdb; pdb.set_trace()
-        url = reverse('snake-names-list', host='api')
+
+class StatusTests(APISubdomainTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.force_authenticate(user=None)
+
+    def test_cannot_read_snake_names(self):
+        url = reverse('snakename-list', host='api')
         response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 401)
+
+
+class EmptyDatabaseSnakeNameTests(APISubdomainTestCase):
+    def test_endpoint_unauthed_returns_empty_list(self):
+        url = reverse('snakename-list', host='api')
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
+
+
+class SnakeNameListTests(APISubdomainTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.snake_python = SnakeName.objects.create(name='Python', scientific='Totally.')
+
+    def test_endpoint_returns_all_snakes(self):
+        url = reverse('snakename-list', host='api')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            [
+                {
+                    'name': 'Python',
+                    'scientific': 'Totally.'
+                }
+            ]
+        )
