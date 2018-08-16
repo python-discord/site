6a18a8dfcf8382b896e28d9bd354196517f654ee
Johannes Christ <jc@jchri.st>
2018-08-16 21:48:42 +0200

    Add the `DocumentationLink` model.

--- api/models.py
+++ api/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class DocumentationLink(models.Model):
+    """A documentation link used by the `!docs` command of the bot."""
+
+    package = models.CharField(primary_key=True, max_length=50)
+    base_url = models.URLField()
+    inventory_url = models.URLField()
+
+
 class SnakeName(models.Model):
+    """A snake name used by the bot's snake cog."""
+
     name = models.CharField(primary_key=True, max_length=100)
     scientific = models.CharField(max_length=150)
