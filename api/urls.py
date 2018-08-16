0b37b3d3782a1506c0b59355d59eb4a036f83e74
Johannes Christ <jc@jchri.st>
2018-08-16 20:27:25 +0200

    Namespace `bot` endpoints.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,18 @@
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
 from .viewsets import SnakeNameViewSet
 
 
 # http://www.django-rest-framework.org/api-guide/routers/#simplerouter
-router = SimpleRouter(trailing_slash=False)
-router.register(r'snake-names', SnakeNameViewSet)
+bot_router = SimpleRouter(trailing_slash=False)
+bot_router.register(r'snake-names', SnakeNameViewSet, base_name='snakename')
 
 app_name = 'api'
-urlpatterns = router.urls
+urlpatterns = (
+    # Build URLs using something like...
+    #
+    # from django_hosts.resolvers import reverse
+    # snake_name_endpoint = reverse('bot:snakename-list', host='api')  # `bot/` endpoints
+    path('bot/', include((bot_router.urls, 'api'), namespace='bot')),
+)
