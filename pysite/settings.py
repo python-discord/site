17bff58a3c8f1a28052f4da88feb13cb2de56922
Johannes Christ <jc@jchri.st>
2018-09-08 00:15:09 +0200

    Use Argon2 for password hashing.

--- pysite/settings.py
+++ pysite/settings.py
@@ -141,6 +141,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
