58333024cd0bae26f322904877c811c58fc1b025
Johannes Christ <jc@jchri.st>
2018-09-21 22:15:54 +0200

    Add trailing slash to admin page.

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView
 app_name = 'home'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
-    path('admin', admin.site.urls)
+    path('admin/', admin.site.urls)
 ]
