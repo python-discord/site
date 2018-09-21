c9c5c60698a233b77813b4d9cf4b8d5506481bc3
Johannes Christ <jc@jchri.st>
2018-09-21 21:47:54 +0200

    Mount `/admin` site at home subdomain.

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
 
 app_name = 'home'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/index.html'), name='index')
+    path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+    path('admin', admin.site.urls)
 ]
