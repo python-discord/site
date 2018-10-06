8b1523b8c484abed432dfd80d4592048e9861405
Johannes Christ <jc@jchri.st>
2018-10-06 22:42:38 +0200

    Update the compose file and dockerapp file.
    The development app from the compose file will now apply migrations on boot and additionally, it will use Django's development server along with the current directory mounted read-only into the container in order to support code reloading, helpful while development.

---
+++ docker/app/migrate_and_serve.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+### NOTE
+# This file is intended to be used by local setups.
+# You do not want to run the Django development server
+# in production. The default Dockerfile command will
+# run using uWSGI, this script is provided purely as
+# a convenience to run migrations and start a development server.
+
+echo [i] Applying migrations.
+python manage.py migrate --verbosity 0
+echo [i] Starting server.
+python manage.py runserver
