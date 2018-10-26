45d5e3a8ad72e771ac6b51003d752b40815c56a0
Johannes Christ <jc@jchri.st>
2018-10-26 19:46:03 +0200

    Drop extra whitespace.

--- pysite/hosts.py
+++ pysite/hosts.py
@@ -4,10 +4,10 @@ from django_hosts import host, patterns
 host_patterns = patterns(
     '',
     # > | Subdomain | URL Module   | Host entry name |
-    host(r'admin', 'admin.urls',  name="admin"),
-    host(r'api',   'api.urls',    name='api'),
+    host(r'admin', 'admin.urls', name="admin"),
+    host(r'api', 'api.urls', name='api'),
     # host(r"staff",    "staff",  name="staff"),
     # host(r"wiki",     "wiki",   name="wiki"),
     # host(r"ws",       "ws",     name="ws"),
-    host(r'.*',       'home.urls',   name=settings.DEFAULT_HOST)
+    host(r'.*', 'home.urls', name=settings.DEFAULT_HOST)
 )
