6fb5329858f030587c90d7981d811f6cbe7fecef
MarkKoz <KozlovMark@gmail.com>
2018-11-21 11:29:15 -0800

    remove prefetch cache invalidation

--- api/viewsets.py
+++ api/viewsets.py
@@ -109,11 +109,6 @@ class InfractionViewSet(CreateModelMixin, RetrieveModelMixin, ListModelMixin, Ge
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
         return Response(serializer.data)
 
 
