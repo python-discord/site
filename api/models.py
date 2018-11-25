04afaefa471d47959fb941cada7a415550090299
Hasan <31344273+ImportErr@users.noreply.github.com>
2018-11-25 09:25:08 +0000

    Add a regex validator to snakename fields. (#151)

--- api/models.py
+++ api/models.py
@@ -92,11 +92,13 @@ class SnakeName(ModelReprMixin, models.Model):
     name = models.CharField(
         primary_key=True,
         max_length=100,
-        help_text="The regular name for this snake, e.g. 'Python'."
+        help_text="The regular name for this snake, e.g. 'Python'.",
+        validators=[RegexValidator(regex=r'^([^0-9])+$')]
     )
     scientific = models.CharField(
         max_length=150,
-        help_text="The scientific name for this snake, e.g. 'Python bivittatus'."
+        help_text="The scientific name for this snake, e.g. 'Python bivittatus'.",
+        validators=[RegexValidator(regex=r'^([^0-9])+$')]
     )
 
     def __str__(self):
