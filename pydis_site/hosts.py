56847b00e6bd090f0a8dbe6421fe773df69b4eeb
Gareth Coles <gdude2002@gmail.com>
2019-04-06 18:35:52 +0100

    Address review by @lemonsaurus

--- pydis_site/hosts.py
+++ pydis_site/hosts.py
@@ -3,11 +3,8 @@ from django_hosts import host, patterns
 
 host_patterns = patterns(
     '',
-    # > | Subdomain | URL Module   | Host entry name |
+    # host(r"subdomain pattern", "URLs module", "host entry name")
     host(r'admin', 'pydis_site.apps.admin.urls', name="admin"),
     host(r'api', 'pydis_site.apps.api.urls', name='api'),
-    # host(r"staff",    "pydis_site.apps.staff",  name="staff"),
-    # host(r"wiki",     "pydis_site.apps.wiki",   name="wiki"),
-    # host(r"ws",       "pydis_site.apps. ws",     name="ws"),
     host(r'.*', 'pydis_site.apps.home.urls', name=settings.DEFAULT_HOST)
 )
