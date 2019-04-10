e00b347ca513e83029110ef740657b2ecda028a2
Johannes Christ <jc@jchri.st>
2019-04-10 19:50:32 +0200

    Document `LogEntryViewSet`.

--- pydis_site/apps/api/viewsets.py
+++ pydis_site/apps/api/viewsets.py
@@ -282,7 +282,33 @@ class InfractionViewSet(CreateModelMixin, RetrieveModelMixin, ListModelMixin, Ge
 
 
 class LogEntryViewSet(CreateModelMixin, GenericViewSet):
-    # TODO: doc me foobar baz boom bang crow caw caw caw
+    """
+    View providing support for creating log entries in the site database
+    for viewing via the log browser.
+
+    ## Routes
+    ### POST /logs
+    Create a new log entry.
+
+    #### Request body
+    >>> {
+    ...     'application': str,  # 'bot' | 'seasonalbot' | 'site'
+    ...     'logger_name': str,  # such as 'bot.cogs.moderation'
+    ...     'timestamp': Optional[str],  # from `datetime.utcnow().isoformat()`
+    ...     'level': str,  # 'debug' | 'info' | 'warning' | 'error' | 'critical'
+    ...     'module': str,  # such as 'pydis_site.apps.api.serializers'
+    ...     'line': int,  # > 0
+    ...     'message': str,  # textual formatted content of the logline
+    ... }
+
+    #### Status codes
+    - 201: returned on success
+    - 400: if the request body has invalid fields, see the response for details
+
+    ## Authentication
+    Requires a API token.
+    """
+
     queryset = LogEntry.objects.all()
     serializer_class = LogEntrySerializer
 
