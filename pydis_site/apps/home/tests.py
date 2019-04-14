b84ebf6e1c813f7973f3eeceafb5c8de4769be3b
Johannes Christ <jc@jchri.st>
2019-04-14 21:57:45 +0200

    Fixup broken reverse in test.

--- pydis_site/apps/home/tests.py
+++ pydis_site/apps/home/tests.py
@@ -4,6 +4,6 @@ from django_hosts.resolvers import reverse
 
 class TestIndexReturns200(TestCase):
     def test_index_returns_200(self):
-        url = reverse('index')
+        url = reverse('home.index')
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
