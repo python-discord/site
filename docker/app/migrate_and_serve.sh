f95ef48e4bad8bd0fb2bc141d5536cd976f01d0b
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2019-04-18 20:43:33 +0200

    Update docker/app/migrate_and_serve.sh
    Co-Authored-By: lemonsaurus <leon.haland@gmail.com>

--- docker/app/migrate_and_serve.sh
+++ docker/app/migrate_and_serve.sh
@@ -14,7 +14,7 @@ echo [i] Collecting static files.
 python manage.py collectstatic --no-input --clear --verbosity 0
 
 echo [i] Creating a superuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin', 'admin')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin', 'admin') if not User.objects.filter(username='admin').exists() else print('Username already exists')" | python manage.py shell
 
 echo [i] Starting server.
 python manage.py runserver 0.0.0.0:8000
