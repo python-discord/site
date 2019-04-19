e43d5f97bb27d13bb75f22edd8fd2e19e2d22f8d
Gareth Coles <gdude2002@gmail.com>
2019-04-19 14:54:48 +0100

    First testing attempt

---
+++ pydis_site/apps/home/tests/test_wiki_templatetags.py
@@ -0,0 +1,39 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+from django.template import Template
+from wiki.models import URLPath as _URLPath
+
+
+URLPath = Mock(_URLPath)
+
+
+class TestURLPathFilter(TestCase):
+    TEMPLATE = Template(
+        """
+        {% load wiki_extra %}
+        {{ obj|render_urlpath }}
+        """
+    )
+
+    def test_str(self):
+        context = {"obj": "path"}
+        rendered = self.TEMPLATE.render(context)
+
+    def test_str_empty(self):
+        context = {"obj": ""}
+        rendered = self.TEMPLATE.render(context)
+
+    def test_urlpath(self):
+        url_path = URLPath()
+        url_path.path = None
+
+        context = {"obj": url_path}
+        rendered = self.TEMPLATE.render(context)
+
+    def test_urlpath_root(self):
+        url_path = URLPath()
+        url_path.path = "/path/"
+
+        context = {"obj": url_path}
+        rendered = self.TEMPLATE.render(context)
