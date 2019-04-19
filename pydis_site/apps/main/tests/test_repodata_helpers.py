2e1bed546023758cfe540b0536244263f68fe67b
Leon Sandøy <leon.sandoy@conmodo.com>
2019-04-19 13:19:12 +0200

    Added test cases to increase coverage for this PR to 100%

--- pydis_site/apps/main/tests/test_repodata_helpers.py
+++ pydis_site/apps/main/tests/test_repodata_helpers.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 from django.conf import settings
 from django.test import TestCase
+from django.utils import timezone
 
 from pydis_site.apps.main.models import RepositoryMetadata
 from pydis_site.apps.main.views import HomeView
@@ -15,6 +18,40 @@ class TestRepositoryMetadataHelpers(TestCase):
 
         self.assertIsInstance(metadata[0], RepositoryMetadata)
         self.assertEquals(len(metadata), len(settings.HOMEPAGE_REPOS))
+
+    def test_returns_cached_metadata(self):
+        """Test if the _get_repo_data helper returns cached data when available."""
+
+        home_view = HomeView()
+        repo_data = RepositoryMetadata(
+            repo_name="python-discord/site",
+            description="testrepo",
+            forks=42,
+            stargazers=42,
+            language="English",
+        )
+        repo_data.save()
+        metadata = home_view._get_repo_data()
+
+        self.assertIsInstance(metadata[0], RepositoryMetadata)
+        print(metadata[0])  # Tests the __str__ in the model
+
+    def test_refresh_stale_metadata(self):
+        """Test if the _get_repo_data helper will refresh when the data is stale"""
+
+        home_view = HomeView()
+        repo_data = RepositoryMetadata(
+            repo_name="python-discord/site",
+            description="testrepo",
+            forks=42,
+            stargazers=42,
+            language="English",
+            last_updated=timezone.now() - timedelta(seconds=121),  # Make the data 2 minutes old.
+        )
+        repo_data.save()
+        metadata = home_view._get_repo_data()
+
+        self.assertIsInstance(metadata[0], RepositoryMetadata)
 
     def test_returns_api_data(self):
         """Tests if the _get_api_data helper returns what it should."""
