5f9dda2db3dacf3c26040e299a555069b2d0655a
Gareth Coles <gdude2002@gmail.com>
2019-04-21 17:04:56 +0100

    Finish linting non-API modules

--- pydis_site/apps/home/apps.py
+++ pydis_site/apps/home/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """Django AppConfig for the home app."""
     name = 'home'
--- pydis_site/apps/home/templatetags/extra_filters.py
+++ pydis_site/apps/home/templatetags/extra_filters.py
@@ -5,4 +5,18 @@ register = template.Library()
 
 @register.filter
 def starts_with(value: str, arg: str):
+    """
+    Simple filter for checking if a string value starts with another string.
+
+    Kind of surprising that Django doesn't come with this one, actually...
+
+    Usage:
+
+    ```django
+        {% if request.url | starts_with:"/wiki" %}
+          ...
+        {% endif %}
+    ```
+    """
+
     return value.startswith(arg)
