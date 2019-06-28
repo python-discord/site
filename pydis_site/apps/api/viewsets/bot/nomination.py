8da441f2b7745b5d5b5f94a76bf4db8341c07e38
SebastiaanZ <33516116+SebastiaanZ@users.noreply.github.com>
2019-06-28 22:24:48 +0200

    Removing unused import

--- pydis_site/apps/api/viewsets/bot/nomination.py
+++ pydis_site/apps/api/viewsets/bot/nomination.py
@@ -3,7 +3,6 @@ from collections import ChainMap
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.mixins import (
