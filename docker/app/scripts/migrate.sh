328f9b9ab42b456001a2b8dab15ca6ebe3ba7923
Leon Sandøy <leon.sandoy@conmodo.com>
2019-07-03 15:58:18 +0200

    Let's increase the verbosity and see what you guys have to say, migrate and collectstatic

--- docker/app/scripts/migrate.sh
+++ docker/app/scripts/migrate.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo --- Applying migrations ---
-python manage.py migrate --verbosity 1
+python manage.py migrate --verbosity 2
 
 echo --- Collecting static files ---
-python manage.py collectstatic --no-input --clear --verbosity 0
+python manage.py collectstatic --no-input --clear --verbosity 2
 
 echo --- Starting uwsgi ---
 exec "$@"  # This runs the CMD at the end of the Dockerfile
