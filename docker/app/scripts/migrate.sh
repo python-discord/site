793249576d26ab448d6f55497a6bef05c8e27ede
Leon Sandøy <leon.sandoy@conmodo.com>
2019-07-03 15:04:02 +0200

    Setting up a method for automatically applying migrations in production. Making a separate Dockerfile for local docker-compose builds.

---
+++ docker/app/scripts/migrate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo --- Applying migrations ---
+python manage.py migrate --verbosity 1
+
+echo --- Collecting static files ---
+python manage.py collectstatic --no-input --clear --verbosity 0
+
+echo --- Starting uwsgi ---
+exec "$@"  # This runs the CMD at the end of the Dockerfile
