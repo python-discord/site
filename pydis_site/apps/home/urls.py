5a1df12dedea6facbc74d81ef70f58139b03e211
Leon Sandøy <leon.sandoy@conmodo.com>
2019-07-06 19:13:54 +0200

    adding hrefs to the more links

--- pydis_site/apps/home/urls.py
+++ pydis_site/apps/home/urls.py
@@ -8,7 +8,7 @@ from .views import HomeView
 app_name = 'home'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('pages/', include('wiki.urls')),
     path('admin/', admin.site.urls),
     path('notifications/', include('django_nyt.urls')),
-    path('wiki/', include('wiki.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
