30b1a1bfa615395a283542fff8cc402e6d3f937f
Johannes Christ <jc@jchri.st>
2019-08-14 21:03:40 +0200

    Apply Mark's suggestions.
    how to logging
    
    Co-Authored-By: Mark <kozlovmark@gmail.com>

--- pydis_site/apps/api/tests/test_dblogger.py
+++ pydis_site/apps/api/tests/test_dblogger.py
@@ -9,7 +9,7 @@ from ..models import LogEntry
 
 class DatabaseLogHandlerTests(TestCase):
     def test_logs_to_database(self):
-        logger = logging.getLogger(__file__)
+        logger = logging.getLogger("test_logger")
         logger.handlers = [DatabaseLogHandler()]
         logger.warning("I am a test case!")
 
@@ -18,9 +18,9 @@ class DatabaseLogHandlerTests(TestCase):
         [entry] = LogEntry.objects.all()
 
         self.assertEqual(entry.application, 'site')
-        self.assertEqual(entry.logger_name, __file__)
+        self.assertEqual(entry.logger_name, "test_logger")
         self.assertIsInstance(entry.timestamp, datetime)
         self.assertEqual(entry.level, 'warning')
-        self.assertEqual(entry.module, 'test_dblogger')
+        self.assertEqual(entry.module, __name__)
         self.assertIsInstance(entry.line, int)
         self.assertEqual(entry.message, "I am a test case!")
