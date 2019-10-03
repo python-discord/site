01c059877bc0d82a72558194e4c8ddb30f1c8b33
Gareth Coles <gdude2002@gmail.com>
2019-10-03 18:53:04 +0100

    Disable Allauth email verification

--- pydis_site/settings.py
+++ pydis_site/settings.py
@@ -364,3 +364,5 @@ AUTHENTICATION_BACKENDS = (
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+
+ACCOUNT_EMAIL_VERIFICATION = "none"  # No verification required; we don't use emails for anything
