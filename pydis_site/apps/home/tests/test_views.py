fa3cc5eb2fe517c24df392ceb9be0bb0e5f6cb77
Gareth Coles <gdude2002@gmail.com>
2019-10-05 14:54:37 +0100

    Test allauth redirect URLs

--- pydis_site/apps/home/tests/test_views.py
+++ pydis_site/apps/home/tests/test_views.py
@@ -7,3 +7,17 @@ class TestIndexReturns200(TestCase):
         url = reverse('home')
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
+
+
+class TestLoginCancelledReturns302(TestCase):
+    def test_login_cancelled_returns_302(self):
+        url = reverse('socialaccount_login_cancelled')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+
+
+class TestLoginErrorReturns302(TestCase):
+    def test_login_error_returns_302(self):
+        url = reverse('socialaccount_login_error')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
