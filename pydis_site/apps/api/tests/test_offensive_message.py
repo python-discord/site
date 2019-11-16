c23d1131fff3ba62431eed0750005b3d5cae9a4c
Akarys42 <Akarys42@users.noreply.github.com>
2019-11-16 18:43:46 +0100

    Write list test for the offensive message model

--- pydis_site/apps/api/tests/test_offensive_message.py
+++ pydis_site/apps/api/tests/test_offensive_message.py
@@ -69,6 +69,45 @@ class CreationTests(APISubdomainTestCase):
                 })
 
 
+class ListTests(APISubdomainTestCase):
+    @classmethod
+    def setUpTestData(cls):  # noqa
+        delete_at = datetime.datetime.now() + datetime.timedelta(days=1)
+        aware_delete_at = delete_at.replace(tzinfo=datetime.timezone.utc)
+
+        cls.messages = [
+            {
+                'id': 602951077675139072,
+                'channel_id': 91284109232308226,
+            },
+            {
+                'id': 645298201494159401,
+                'channel_id': 592000283102674944
+            }
+        ]
+
+        cls.of1 = OffensiveMessage.objects.create(
+            **cls.messages[0],
+            delete_date=aware_delete_at.isoformat()
+        )
+        cls.of2 = OffensiveMessage.objects.create(
+            **cls.messages[1],
+            delete_date=aware_delete_at.isoformat()
+        )
+
+        # Expected API answer :
+        cls.messages[0]['delete_date'] = delete_at.isoformat() + 'Z'
+        cls.messages[1]['delete_date'] = delete_at.isoformat() + 'Z'
+
+    def test_get_data(self):
+        url = reverse('bot:offensivemessage-list', host='api')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.json(), self.messages)
+
+
 class DeletionTests(APISubdomainTestCase):
     @classmethod
     def setUpTestData(cls):  # noqa
