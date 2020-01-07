647a3576402cbcb83f2998e0277c2d4774d55ee8
bendiller <diller.benjamin@gmail.com>
2020-01-07 03:14:56 -0700

    Implement EmptyDatabaseReminderAPITests

--- pydis_site/apps/api/tests/test_reminders.py
+++ pydis_site/apps/api/tests/test_reminders.py
@@ -25,3 +25,18 @@ class UnauthedReminderAPITests(APISubdomainTestCase):
         response = self.client.delete(url)
 
         self.assertEqual(response.status_code, 401)
+
+
+class EmptyDatabaseReminderAPITests(APISubdomainTestCase):
+    def test_list_all_returns_empty_list(self):
+        url = reverse('bot:reminder-list', host='api')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
+
+    def test_delete_returns_404(self):
+        url = reverse('bot:reminder-detail', args=('1234',), host='api')
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, 404)
