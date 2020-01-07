2552f22a25913adbbc024dfe6b8948b5e7acd18f
bendiller <diller.benjamin@gmail.com>
2020-01-07 03:14:22 -0700

    Instantiate test_reminders module; implement UnauthedReminderAPITests

---
+++ pydis_site/apps/api/tests/test_reminders.py
@@ -0,0 +1,27 @@
+from django_hosts.resolvers import reverse
+
+from .base import APISubdomainTestCase
+
+
+class UnauthedReminderAPITests(APISubdomainTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.force_authenticate(user=None)
+
+    def test_list_returns_401(self):
+        url = reverse('bot:reminder-list', host='api')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_create_returns_401(self):
+        url = reverse('bot:reminder-list', host='api')
+        response = self.client.post(url, data={'not': 'important'})
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_delete_returns_401(self):
+        url = reverse('bot:reminder-detail', args=('1234',), host='api')
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, 401)
