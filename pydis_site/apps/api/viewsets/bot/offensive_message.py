f13f8b8852bf80836f2ad69b4e077af7b376a3ad
Matteo Bertucci <matteobertucci2004@gmail.com>
2020-04-06 14:17:53 +0200

    Allow PATCH requests on the OffensiveMessage model
    This is required in order to reschedule failed message deletions

--- pydis_site/apps/api/viewsets/bot/offensive_message.py
+++ pydis_site/apps/api/viewsets/bot/offensive_message.py
@@ -1,7 +1,8 @@
 from rest_framework.mixins import (
     CreateModelMixin,
     DestroyModelMixin,
-    ListModelMixin
+    ListModelMixin,
+    UpdateModelMixin
 )
 from rest_framework.viewsets import GenericViewSet
 
@@ -10,7 +11,7 @@ from pydis_site.apps.api.serializers import OffensiveMessageSerializer
 
 
 class OffensiveMessageViewSet(
-    CreateModelMixin, ListModelMixin, DestroyModelMixin, GenericViewSet
+    CreateModelMixin, ListModelMixin, DestroyModelMixin, UpdateModelMixin, GenericViewSet
 ):
     """
     View providing CRUD access to offensive messages.
@@ -45,6 +46,20 @@ class OffensiveMessageViewSet(
     #### Status codes
     - 201: returned on success
     - 400: if the body format is invalid
+
+    ### POST /bot/offensive-messages/<id:int>
+    Edit an existing offensive message object with the given `id`.
+
+    #### Request body
+    >>> {
+    ...     'channel_id': int
+    ...     'delete_date': datetime.datetime  # ISO-8601-formatted date
+    ... }
+
+    #### Status codes
+    - 200: returned on success
+    - 400: if the body format is invalid
+    - 404: if the offensive message object with the given `id` does not exist
 
     ### DELETE /bot/offensive-messages/<id:int>
     Delete the offensive message object with the given `id`.
