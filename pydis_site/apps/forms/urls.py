59e5e77efa257e9b48209e30724b384e2d3a5f10
Joseph Banks <joseph@josephbanks.me>
2020-06-20 05:27:09 +0100

    Add index and edit to urls.py

--- pydis_site/apps/forms/urls.py
+++ pydis_site/apps/forms/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from .views import edit, index
+
+
 app_name = 'forms'
 
-from . import views
-
 urlpatterns = [
-    path('hello', views.hello)
+    path('', index.FormIndexView.as_view(), name="forms_index"),
+    path('edit/<str:form_id>', edit.FormEditView.as_view(), name="edit_form")
 ]
