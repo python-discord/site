26f87ff97dd0855a2796002eb750c77331889b79
Joseph Banks <joseph@josephbanks.me>
2020-06-21 00:00:31 +0100

    Add questions update view

--- pydis_site/apps/forms/urls.py
+++ pydis_site/apps/forms/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import edit, index
+from .views import edit, index, questions
 
 
 app_name = 'forms'
 
 urlpatterns = [
     path('', index.FormIndexView.as_view(), name="forms_index"),
-    path('edit/<str:form_id>', edit.FormEditView.as_view(), name="edit_form")
+    path('edit/<str:form_id>', edit.FormEditView.as_view(), name="edit_form"),
+    path('questions/<str:form_id>', questions.FormQuestionsView.as_view(), name="update_questions"),
 ]
