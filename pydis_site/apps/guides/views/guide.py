1b11ad2fd181edd87a29834544112b02a884b04f
ks129 <45097959+ks129@users.noreply.github.com>
2020-09-20 11:40:45 +0300

    Add codeblock parsing to Markdown parser for guide

--- pydis_site/apps/guides/views/guide.py
+++ pydis_site/apps/guides/views/guide.py
@@ -28,7 +28,7 @@ class GuideView(View):
         if not os.path.exists(path) or not os.path.isfile(path):
             raise Http404(f"Guide not found {path}")
 
-        md = Markdown(extensions=['meta', 'attr_list'])
+        md = Markdown(extensions=['meta', 'attr_list', 'fenced_code'])
         with open(path) as f:
             html = md.convert(f.read())
             f.close()
