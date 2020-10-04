638c323f3eae4a35f6e8ab4e4634fb30e5e9e163
ks129 <45097959+ks129@users.noreply.github.com>
2020-10-04 14:50:17 +0300

    Simplify resources index view

--- pydis_site/apps/resources/views/resources.py
+++ pydis_site/apps/resources/views/resources.py
@@ -1,12 +1,7 @@
-from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
+    
+class ResourcesView(TemplateView):
+    """View for resources index page."""
 
-class ResourcesView(View):
-    """Handles base resources page that shows different resource types."""
-
-    def get(self, request: WSGIRequest) -> HttpResponse:
-        """Show base resources page."""
-        return render(request, "resources/resources.html")
+    template_name = "resources/resources.html"
