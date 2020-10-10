ad5d9b3c17c2b9f84ffa8ff01e91122374942b83
RohanJnr <rohanjnr44@gmail.com>
2020-10-10 21:54:07 +0530

    Add UserEvent model representing a user event in the server

---
+++ pydis_site/apps/api/models/bot/user_event.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from pydis_site.apps.api.models.bot.user import User
+from pydis_site.apps.api.models.mixins import ModelReprMixin
+
+
+class UserEvent(ModelReprMixin, models.Model):
+    """A user event in the server."""
+
+    name = models.CharField(
+        max_length=128,
+        verbose_name="Event Name",
+        help_text="Name of the user event.",
+        unique=True
+    )
+    organizer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    subscriptions = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name="subscriptions"
+    )
