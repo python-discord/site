99f48fb12dced0f011b2be26cbed9d5c451ff0b5
ks129 <45097959+ks129@users.noreply.github.com>
2020-10-26 19:43:35 +0200

    Create basic events app

---
+++ pydis_site/apps/events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+
+from pydis_site.apps.events.views import IndexView, PageView
+
+app_name = "events"
+urlpatterns = [
+    path("", IndexView.as_view(), name="events"),
+    re_path("(?P<path>.+)/$", PageView.as_view(), name="page"),
+]
