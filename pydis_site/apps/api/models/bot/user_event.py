374b31e6c77543d4f4ae7eb213158c5039291d4a
RohanJnr <rohanjnr44@gmail.com>
2020-10-27 00:29:17 +0530

    add message_id field and make description a required field

--- pydis_site/apps/api/models/bot/user_event.py
+++ pydis_site/apps/api/models/bot/user_event.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from pydis_site.apps.api.models.bot.user import User
@@ -8,17 +9,29 @@ class UserEvent(ModelReprMixin, models.Model):
     """A user event in the server."""
 
     name = models.CharField(
-        max_length=128,
+        max_length=64,
         verbose_name="Event Name",
         help_text="Name of the user event.",
-        unique=True
+        primary_key=True
     )
     organizer = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        help_text="The event organizer."
     )
-    description = models.TextField(
-        blank=True
+    description = models.TextField()
+    message_id = models.BigIntegerField(
+        help_text=(
+            "The message ID of the message "
+            "sent in user events channel."
+        ),
+        unique=True,
+        validators=(
+            MinValueValidator(
+                limit_value=0,
+                message="message IDs cannot be negative."
+            ),
+        )
     )
     subscriptions = models.ManyToManyField(
         User,
