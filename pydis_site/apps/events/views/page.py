fcfcdd5463602eaedfe3ee13c45da248b1296923
ks129 <45097959+ks129@users.noreply.github.com>
2020-11-08 19:05:06 +0200

    Use path in variable instead getting it every time in kwargs

--- pydis_site/apps/events/views/page.py
+++ pydis_site/apps/events/views/page.py
@@ -10,15 +10,16 @@ class PageView(TemplateView):
 
     def get_template_names(self) -> List[str]:
         """Get specific template names."""
-        page_path = settings.PAGES_PATH / self.kwargs['path']
+        path: str = self.kwargs['path']
+        page_path = settings.PAGES_PATH / path
         if page_path.is_dir():
             page_path = page_path / "_index.html"
-            self.kwargs['path'] = f"{self.kwargs['path']}/_index.html"
+            path = f"{path}/_index.html"
         else:
-            page_path = settings.PAGES_PATH / f"{self.kwargs['path']}.html"
-            self.kwargs['path'] = f"{self.kwargs['path']}.html"
+            page_path = settings.PAGES_PATH / f"{path}.html"
+            path = f"{path}.html"
 
         if not page_path.exists():
             raise Http404
 
-        return [f"events/{settings.PAGES_PATH.name}/{self.kwargs['path']}"]
+        return [f"events/{settings.PAGES_PATH.name}/path}"]
