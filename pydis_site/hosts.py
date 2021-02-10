e5a0bba5bda972439f2cda87f3966b19e4cd10a5
Joe Banks <joseph@josephbanks.me>
2021-02-10 00:54:58 +0000

    Don't change the name of the host for subdomains
    The host is used to build reverse URLs during tests, changing it causes it to fail.

--- pydis_site/hosts.py
+++ pydis_site/hosts.py
@@ -5,7 +5,7 @@ host_patterns = patterns(
     '',
     host(r'admin', 'pydis_site.apps.admin.urls', name="admin"),
     # External API ingress (over the net)
-    host(r'api', 'pydis_site.apps.api.urls', name='external_api'),
+    host(r'api', 'pydis_site.apps.api.urls', name='api'),
     # Internal API ingress (cluster local)
     host(r'pydis-api', 'pydis_site.apps.api.urls', name='internal_api'),
     host(r'staff', 'pydis_site.apps.staff.urls', name='staff'),
