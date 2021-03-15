bc66963ff6d5dcb64e08c8cc35bd18fbedf2f025
Boris Muratov <8bee278@gmail.com>
2021-03-16 01:41:53 +0200

    Add more records to test the endpoint with

--- postgres/init.sql
+++ postgres/init.sql
@@ -13,6 +13,11 @@ INSERT INTO users VALUES (
     current_timestamp
 );
 
+INSERT INTO users VALUES (
+    1,
+    current_timestamp
+);
+
 CREATE TABLE channels (
     id varchar,
     name varchar,
@@ -22,6 +27,36 @@ CREATE TABLE channels (
 INSERT INTO channels VALUES(
     '267659945086812160',
     'python-general'
+);
+
+INSERT INTO channels VALUES(
+    '11',
+    'help-apple'
+);
+
+INSERT INTO channels VALUES(
+    '12',
+    'help-cherry'
+);
+
+INSERT INTO channels VALUES(
+    '21',
+    'ot0-hello'
+);
+
+INSERT INTO channels VALUES(
+    '22',
+    'ot1-world'
+);
+
+INSERT INTO channels VALUES(
+    '31',
+    'voice-chat-0'
+);
+
+INSERT INTO channels VALUES(
+    '32',
+    'code-help-voice-0'
 );
 
 INSERT INTO channels VALUES(
@@ -52,4 +87,60 @@ INSERT INTO messages VALUES(
     false,
     now() + INTERVAL '10 minutes,',
     '1234'
+);
+
+INSERT INTO messages VALUES(
+    2,
+    0,
+    false,
+    now(),
+    '11'
+);
+
+INSERT INTO messages VALUES(
+    3,
+    0,
+    false,
+    now(),
+    '12'
+);
+
+INSERT INTO messages VALUES(
+    4,
+    1,
+    false,
+    now(),
+    '21'
+);
+
+INSERT INTO messages VALUES(
+    5,
+    1,
+    false,
+    now(),
+    '22'
+);
+
+INSERT INTO messages VALUES(
+    6,
+    1,
+    false,
+    now(),
+    '31'
+);
+
+INSERT INTO messages VALUES(
+    7,
+    1,
+    false,
+    now(),
+    '32'
+);
+
+INSERT INTO messages VALUES(
+    8,
+    1,
+    true,
+    now(),
+    '32'
 );
