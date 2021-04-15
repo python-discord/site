61c48e9bbce7b3a4e683adb08072619a565b38bf
ks129 <45097959+ks129@users.noreply.github.com>
2021-04-15 16:26:47 +0300

    Update redirect app tests to take account recent changes

--- pydis_site/apps/redirect/tests.py
+++ pydis_site/apps/redirect/tests.py
@@ -2,9 +2,13 @@ from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
 
+TESTING_ARGUMENTS = {"resources_resources_redirect": ("reading",)}
+
 
 class RedirectTests(TestCase):
-    def test_redirects(self):
+    """Survival tests for redirects."""
+
+    def test_redirects(self) -> None:
         """
         Should redirect to given route based on redirect rules.
 
@@ -12,9 +16,28 @@ class RedirectTests(TestCase):
         1. Redirects only once.
         2. Redirects to right URL.
         """
-        for original_path, (redirect_route, name, args) in settings.REDIRECTIONS.items():
-            with self.subTest(original_path=original_path, redirect_route=redirect_route, name=name, args=args):
-                resp = self.client.get(reverse(f"home:redirect:{name}", args=args), follow=True)
+        for original_path, (redirect_route, name, static_args) in settings.REDIRECTIONS.items():
+            with self.subTest(
+                    original_path=original_path,
+                    redirect_route=redirect_route,
+                    name=name,
+                    static_args=static_args,
+                    args=TESTING_ARGUMENTS.get(name, ())
+            ):
+                resp = self.client.get(
+                    reverse(
+                        f"home:redirect:{name}",
+                        args=TESTING_ARGUMENTS.get(name, ())
+                    ),
+                    follow=True
+                )
 
                 self.assertEqual(1, len(resp.redirect_chain))
-                self.assertRedirects(resp, reverse(f"home:{redirect_route}", args=args))
+                self.assertRedirects(
+                    resp,
+                    reverse(
+                        f"home:{redirect_route}",
+                        args=TESTING_ARGUMENTS.get(name, ()) + static_args
+                    ),
+                    status_code=301
+                )
