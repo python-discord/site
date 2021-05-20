40ccb3a1dabaaa888c81245984d9103981c27986
Vivaan Verma <vivaan.verma@gmail.com>
2021-05-20 18:20:45 +0100

    Updated custom template tag to not throw 404s

--- pydis_site/apps/content/templatetags/page_github.py
+++ pydis_site/apps/content/templatetags/page_github.py
@@ -1,11 +1,16 @@
 from django import template
+import requests
 
 register = template.Library()
 
 
 @register.filter()
 def page_github(current_uri: str):
-    return current_uri.replace(
-        "https://pythondiscord.com/pages/",
-        "https://github.com/python-discord/site/tree/main/pydis_site/apps/content/resources"
+    github_uri = current_uri.replace(
+        "http://pythondiscord.local:8000/pages/",
+        "https://github.com/python-discord/site/tree/main/pydis_site/apps/content/resources/"
     )
+    github_uri = github_uri[:-1] if github_uri.endswith("/") else github_uri
+    if requests.get(github_uri).status_code == 404:
+        github_uri += ".md"
+    return github_uri
