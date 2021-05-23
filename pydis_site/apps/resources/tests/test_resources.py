7c67ea311eebeb5273d02469295fa4218348e213
Leon Sandøy <leon.sandoy@conmodo.com>
2021-05-23 10:51:55 +0200

    Test boilerplate

---
+++ pydis_site/apps/resources/tests/test_resources.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestResources(TestCase):
+    """Test our resource filtering systems."""
+
+    def test_utils_to_retrieve_tags(self):
+        """Test that the utils that retrieve the tags work as intended."""
+        pass
