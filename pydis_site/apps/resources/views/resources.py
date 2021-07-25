0d55308776a029517899f8f26d454b0bb920a9ee
swfarnsworth <swfarnsworth@gmail.com>
2021-07-24 20:33:11 -0400

    Added type annotations; refactored.

--- pydis_site/apps/resources/views/resources.py
+++ pydis_site/apps/resources/views/resources.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from pydis_site.apps.resources.utils import get_resources_meta
@@ -5,20 +6,21 @@ from pydis_site.apps.resources.utils import get_resources_meta
 RESOURCE_META_TAGS = get_resources_meta()
 
 
-def format_checkbox_options(options: str) -> list:
+def format_checkbox_options(options: str) -> list[str]:
     """Split up the comma separated parameters into a list."""
-    if options:
-        return options.split(",")[:-1]
-    return list()
+    return options.split(",")[:-1] if options else []
 
 
-def resource_view(request):
+def resource_view(request: HttpRequest) -> HttpResponse:
     """View for resources index page."""
-    context = {
-        "checkboxOptions": format_checkbox_options(request.GET.get("checkboxOptions")),
-        "topics": RESOURCE_META_TAGS.get("topics"),
-        "tag_types": RESOURCE_META_TAGS.get("type"),
-        "payment_tiers": RESOURCE_META_TAGS.get("payment_tiers"),
-        "complexities": RESOURCE_META_TAGS.get("complexity")
-    }
-    return render(request, template_name="resources/resources.html", context=context)
+    return render(
+        request,
+        template_name="resources/resources.html",
+        context={
+            "checkboxOptions": format_checkbox_options(request.GET.get("checkboxOptions")),
+            "topics": RESOURCE_META_TAGS.get("topics"),
+            "tag_types": RESOURCE_META_TAGS.get("type"),
+            "payment_tiers": RESOURCE_META_TAGS.get("payment_tiers"),
+            "complexities": RESOURCE_META_TAGS.get("complexity")
+        }
+    )
