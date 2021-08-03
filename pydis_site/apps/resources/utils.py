7cd0f11993701fb360b392911520f6b2839b8ed1
swfarnsworth <swfarnsworth@gmail.com>
2021-08-03 12:09:35 -0400

    Delete unused `yaml_file_matches_search` method.

--- pydis_site/apps/resources/utils.py
+++ pydis_site/apps/resources/utils.py
@@ -1,7 +1,6 @@
 import typing as t
 from collections import defaultdict
 from functools import reduce
-from itertools import chain
 from operator import and_, or_
 from pathlib import Path
 
@@ -31,13 +30,6 @@ for name, resource in RESOURCES.items():
     for category, tags in resource['tags'].items():
         for tag in tags:
             RESOURCE_TABLE[category][_transform_name(tag)].add(name)
-
-
-def yaml_file_matches_search(yaml_data: dict, search_terms: list[str]) -> bool:
-    """Checks which resources contain tags for every search term passed."""
-    search_terms = [x.lower() for x in search_terms]
-    matching_tags = [x for x in chain(*yaml_data["tags"].values()) if x in search_terms]
-    return len(matching_tags) >= len(search_terms)
 
 
 def get_resources_from_search(search_categories: dict[str, set[str]]) -> list[Resource]:
