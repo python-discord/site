6bd72f73b761264032907530a9ab1d4c4fa0e97f
Joe Banks <joe@jb3.dev>
2021-09-03 22:40:38 +0100

    Ignore gunicorn configuration from flake8

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,10 @@
-# Code taken from https://github.com/prometheus/client_python#multiprocess-mode-eg-gunicorn
+"""
+Configuration file for gunicorn.
+
+Code taken from https://github.com/prometheus/client_python#multiprocess-mode-eg-gunicorn
+"""
 from prometheus_client import multiprocess
 
-def child_exit(server, worker):
+
+def child_exit(server, worker) -> None:
     multiprocess.mark_process_dead(worker.pid)
