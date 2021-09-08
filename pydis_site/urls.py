d84e2e131ea467cf9e83f68570f1ab262447bd2d
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-09-08 16:17:28 +0200

    URLs: add an allow everything robots.txt
    This commit simply adds a `robots.txt` file from within the URL configuration.

--- pydis_site/urls.py
+++ pydis_site/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include, path
 
 
+def robots_txt(request: WSGIRequest) -> HttpResponse:
+    """Return an allow everything robots.txtfile."""
+    return HttpResponse("User-agent: *\nAllow: /", headers={"Content-type": "text/plain"})
+
+
 urlpatterns = (
     path('', include('pydis_site.apps.home.urls', namespace='home')),
+    path('robots.txt', robots_txt),
     path('staff/', include('pydis_site.apps.staff.urls', namespace='staff')),
 )
