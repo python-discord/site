2fde6799fc5211658129cac3271241f53e17967b
Hassan Abouelela <hassan@hassanamr.com>
2021-10-05 01:40:04 +0400

    Fixes Broken Content Pages
    Signed-off-by: Hassan Abouelela <hassan@hassanamr.com>

--- pydis_site/apps/content/urls.py
+++ pydis_site/apps/content/urls.py
@@ -8,24 +8,28 @@ from . import views
 app_name = "content"
 
 
-def __get_all_folders(root: Path, folder: typing.Optional[Path] = None) -> list[str]:
-    """Find all folders recursively within `folder`."""
+def __get_all_files(root: Path, folder: typing.Optional[Path] = None) -> list[str]:
+    """Find all folders and markdown files recursively starting from `root`."""
     if not folder:
         folder = root
 
-    folders = []
+    results = []
 
     for sub_folder in folder.iterdir():
         if sub_folder.is_dir():
-            folders.append(sub_folder.relative_to(root).__str__().replace("\\", "/"))
-            folders.extend(__get_all_folders(root, sub_folder))
+            results.append(sub_folder.relative_to(root).__str__().replace("\\", "/"))
+            results.extend(__get_all_files(root, sub_folder))
+        else:
+            path, extension = sub_folder.relative_to(root).__str__().rsplit(".", maxsplit=1)
+            if extension == "md":
+                results.append(path.replace("\\", "/"))
 
-    return folders
+    return results
 
 
 def get_all_pages() -> typing.Iterator[dict[str, str]]:
     """Yield a dict of all pag categories."""
-    for location in __get_all_folders(Path("pydis_site", "apps", "content", "resources")):
+    for location in __get_all_files(Path("pydis_site", "apps", "content", "resources")):
         yield {"location": location}
 
 
