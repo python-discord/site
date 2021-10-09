247c4ba220f9ee73767f4aac2737e520e7ec4b07
Hassan Abouelela <hassan@hassanamr.com>
2021-10-10 01:30:06 +0300

    Adds Static Route Configurations
    Adds configuration which specifies how routes should be handled when
    building a static preview.

--- pydis_site/urls.py
+++ pydis_site/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from pydis_site import settings
 
-urlpatterns = (
+NON_STATIC_PATTERNS = [
     path('admin/', admin.site.urls),
 
     # External API ingress (over the net)
@@ -14,9 +15,15 @@ urlpatterns = (
     # from wildcard matching all requests to `pages/...`.
     path('', include('pydis_site.apps.redirect.urls')),
     path('', include('django_prometheus.urls')),
+
+    path('staff/', include('pydis_site.apps.staff.urls', namespace='staff')),
+] if not settings.env("STATIC_BUILD") else []
+
+
+urlpatterns = (
+    *NON_STATIC_PATTERNS,
     path('pages/', include('pydis_site.apps.content.urls', namespace='content')),
     path('resources/', include('pydis_site.apps.resources.urls')),
     path('events/', include('pydis_site.apps.events.urls', namespace='events')),
-    path('staff/', include('pydis_site.apps.staff.urls', namespace='staff')),
     path('', include('pydis_site.apps.home.urls', namespace='home')),
 )
