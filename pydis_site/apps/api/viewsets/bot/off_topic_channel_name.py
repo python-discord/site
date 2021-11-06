4377c6d960e3fbf3a416230d1ad7de972019898e
RohanJnr <rohanjnr44@gmail.com>
2021-11-06 14:43:17 +0530

    Remove get_queryset() and add new class variable .

--- pydis_site/apps/api/viewsets/bot/off_topic_channel_name.py
+++ pydis_site/apps/api/viewsets/bot/off_topic_channel_name.py
@@ -57,6 +57,7 @@ class OffTopicChannelNameViewSet(ModelViewSet):
 
     lookup_field = 'name'
     serializer_class = OffTopicChannelNameSerializer
+    queryset = OffTopicChannelName.objects.all()
 
     def get_object(self) -> OffTopicChannelName:
         """
@@ -64,11 +65,10 @@ class OffTopicChannelNameViewSet(ModelViewSet):
 
         If it doesn't, a HTTP 404 is returned by way of throwing an exception.
         """
-        queryset = self.get_queryset()
         name = self.kwargs[self.lookup_field]
-        return get_object_or_404(queryset, name=name)
+        return get_object_or_404(self.queryset, name=name)
 
-    def get_queryset(self, **kwargs) -> QuerySet:
+    def get_queryset(self) -> QuerySet:
         """Returns a queryset that covers the entire OffTopicChannelName table."""
         return OffTopicChannelName.objects.all()
 
@@ -108,13 +108,13 @@ class OffTopicChannelNameViewSet(ModelViewSet):
                     'random_items': ["Must be a positive integer."]
                 })
 
-            queryset = self.get_queryset().order_by('used', '?')[:random_count]
+            queryset = self.queryset.order_by('used', '?')[:random_count]
 
             # When any name is used in our listing then this means we reached end of round
             # and we need to reset all other names `used` to False
             if any(offtopic_name.used for offtopic_name in queryset):
                 # These names that we just got have to be excluded from updating used to False
-                self.get_queryset().update(
+                self.queryset.update(
                     used=Case(
                         When(
                             name__in=(offtopic_name.name for offtopic_name in queryset),
@@ -125,7 +125,7 @@ class OffTopicChannelNameViewSet(ModelViewSet):
                 )
             else:
                 # Otherwise mark selected names `used` to True
-                self.get_queryset().filter(
+                self.queryset.filter(
                     name__in=(offtopic_name.name for offtopic_name in queryset)
                 ).update(used=True)
 
@@ -137,6 +137,6 @@ class OffTopicChannelNameViewSet(ModelViewSet):
         if active_param := request.query_params.get("active"):
             params["active"] = active_param.lower() == "true"
 
-        queryset = self.get_queryset().filter(**params)
+        queryset = self.queryset.filter(**params)
         serialized = self.serializer_class(queryset, many=True)
         return Response(serialized.data)
