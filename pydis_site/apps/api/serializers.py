546a2d18bb52a0b4f1fc31de765119623a97f7fa
Izan <tizzysaurus@gmail.com>
2021-11-25 10:44:06 +0000

    Fix linting

--- pydis_site/apps/api/serializers.py
+++ pydis_site/apps/api/serializers.py
@@ -145,7 +145,16 @@ class InfractionSerializer(ModelSerializer):
 
         model = Infraction
         fields = (
-            'id', 'inserted_at', 'expires_at', 'active', 'user', 'actor', 'type', 'reason', 'hidden', 'dm_sent'
+            'id',
+            'inserted_at',
+            'expires_at',
+            'active',
+            'user',
+            'actor',
+            'type',
+            'reason',
+            'hidden',
+            'dm_sent'
         )
         validators = [
             UniqueTogetherValidator(
