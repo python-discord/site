71b1e7ec1ed5fd8f445015a759d74bfdd8828986
Leon Sandøy <leon.haland@gmail.com>
2022-01-31 22:29:18 +0100

    Revert changes to pyproject.toml.
    These changes were not necessary. I am very sorry for the things
    that I did.

--- pydis_site/apps/resources/tests/test_views.py
+++ pydis_site/apps/resources/tests/test_views.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from django.conf import settings
 from django.test import TestCase
-from django_hosts import reverse
+from django.urls import reverse
 
 TESTING_RESOURCES_PATH = Path(
     settings.BASE_DIR, "pydis_site", "apps", "resources", "tests", "testing_resources"
