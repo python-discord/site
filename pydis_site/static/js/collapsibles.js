e07d21a91e63353d693a6fcee3eef0a16b593e06
Leon Sandøy <leon.haland@gmail.com>
2022-02-01 02:50:27 +0100

    Fix delay when collapsing collapsibles.

--- pydis_site/static/js/collapsibles.js
+++ pydis_site/static/js/collapsibles.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Set maxHeight to scroll height on all matching collapsibles
+    const contentContainers = document.getElementsByClassName("collapsible-content");
+    for (const container of contentContainers) {
+        container.style.maxHeight = container.scrollHeight + "px";
+    }
+
     const headers = document.getElementsByClassName("collapsible");
     for (const header of headers) {
         header.addEventListener("click", () => {
