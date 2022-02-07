58fa2d25395f01e379956379ba8ce55d5ff2b7f7
GDWR <gregory.dwr@gmail.com>
2022-02-07 22:22:42 +0000

    Correct typehints

--- pydis_site/context_processors.py
+++ pydis_site/context_processors.py
@@ -1,8 +1,8 @@
-from django.template import RequestContext
+from django.http import HttpRequest
 
 from pydis_site.constants import GIT_SHA
 
 
-def git_sha_processor(_: RequestContext) -> dict:
+def git_sha_processor(_: HttpRequest) -> dict:
     """Expose the git SHA for this repo to all views."""
     return {'git_sha': GIT_SHA}
