d2d98a1a3ce7a0def73e75be6e305e03b1af864e
GDWR <gregory.dwr@gmail.com>
2022-02-08 23:44:26 +0000

    Determine current theme and set switch

--- pydis_site/static/js/base/navbar.js
+++ pydis_site/static/js/base/navbar.js
@@ -44,12 +44,39 @@ function setStyleSheets() {
     }
 }
 
+function toggleThemeSwitch() {
+    let switchToggle = $(".switch")[0];
+    let knob = $(".knob")[0];
+
+    if (knob.classList.contains("dark")) {
+        knob.classList.remove("dark");
+        knob.classList.add("light");
+
+        // After 500ms, switch the icons
+        setTimeout(function() {
+            switchToggle.classList.remove("dark");
+            switchToggle.classList.add("light");
+        }, 100);
+    } else {
+        knob.classList.remove("light");
+        knob.classList.add("dark");
+
+        // After 500ms, switch the icons
+        setTimeout(function() {
+            switchToggle.classList.remove("light");
+            switchToggle.classList.add("dark");
+        }, 100);
+    }
+}
 
 // Executed when the page has finished loading.
 document.addEventListener("DOMContentLoaded", () => {
 
     setStyleSheets();
     displayThemedElements();
+
+    if (getCurrentTheme() === "default")
+        toggleThemeSwitch();
 
     $('#theme-switch').on("click", () => {
 
@@ -62,29 +89,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         setStyleSheets();
         displayThemedElements();
-
-        // Animations
-        let switchToggle = $(".switch")[0];
-        let knob = $(".knob")[0];
-
-        if (knob.classList.contains("dark")) {
-            knob.classList.remove("dark");
-            knob.classList.add("light");
-
-            // After 500ms, switch the icons
-            setTimeout(function() {
-                switchToggle.classList.remove("dark");
-                switchToggle.classList.add("light");
-            }, 100);
-        } else {
-            knob.classList.remove("light");
-            knob.classList.add("dark");
-
-            // After 500ms, switch the icons
-            setTimeout(function() {
-                switchToggle.classList.remove("light");
-                switchToggle.classList.add("dark");
-            }, 100);
-        }
+        toggleThemeSwitch();
     });
 });
