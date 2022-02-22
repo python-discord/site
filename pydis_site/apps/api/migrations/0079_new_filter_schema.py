01ccc1dac80cc2958849d5be90255294f38878fb
mbaruh <mbaruh@hotmail.com>
2022-02-22 20:44:24 +0200

    Changed channeld fields to char arrays, merged migrations
    - The fields concerning channels were changed to contains strings instead of integers in order to allow specifying channels and categories by name.
    
    The migrations were merged into a single migration.

--- pydis_site/apps/api/migrations/0079_new_filter_schema.py
+++ pydis_site/apps/api/migrations/0079_new_filter_schema.py
@@ -9,7 +9,14 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 import pydis_site.apps.api.models.bot.filters
 
-OLD_LIST_NAMES = (('GUILD_INVITE', 'ALLOW'), ('FILE_FORMAT', 'DENY'), ('DOMAIN_NAME', 'DENY'), ('FILTER_TOKEN', 'DENY'))
+OLD_LIST_NAMES = (('GUILD_INVITE', 'ALLOW'), ('FILE_FORMAT', 'DENY'), ('DOMAIN_NAME', 'DENY'), ('FILTER_TOKEN', 'DENY'), ('REDIRECT', 'DENY'))
+change_map = {
+    "FILTER_TOKEN": "token",
+    "DOMAIN_NAME": "domain",
+    "GUILD_INVITE": "invite",
+    "FILE_FORMAT": "extension",
+    "REDIRECT": "redirect"
+}
 
 
 def forward(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
@@ -28,22 +35,23 @@ def forward(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
             dm_content = ""
 
         list_ = filter_list.objects.create(
-            name=name.lower(),
+            name=change_map[name],
             list_type=1 if type_ == "ALLOW" else 0,
-            ping_type=(["onduty"] if name != "FILE_FORMAT" else []),
+            ping_type=(["Moderators"] if name != "FILE_FORMAT" else []),
             filter_dm=True,
             dm_ping_type=[],
             delete_messages=(True if name != "FILTER_TOKEN" else False),
-            bypass_roles=["staff"],
+            bypass_roles=["Helpers"],
             enabled=True,
             dm_content=dm_content,
+            dm_embed="",
             infraction_type="",
             infraction_reason="",
             infraction_duration=timedelta(seconds=0),
-            disallowed_channels=[],
-            disallowed_categories=[],
-            allowed_channels=[],
-            allowed_categories=[]
+            disabled_channels=[],
+            disabled_categories=(["CODE JAM"] if name in ("FILE_FORMAT", "GUILD_INVITE") else []),
+            enabled_channels=[],
+            send_alert=(name in ('GUILD_INVITE', 'DOMAIN_NAME', 'FILTER_TOKEN'))
         )
 
         for object_ in objects:
@@ -59,13 +67,14 @@ def forward(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
                 bypass_roles=None,
                 enabled=None,
                 dm_content=None,
+                dm_embed=None,
                 infraction_type=None,
                 infraction_reason=None,
                 infraction_duration=None,
-                disallowed_channels=None,
-                disallowed_categories=None,
-                allowed_channels=None,
-                allowed_categories=None
+                disabled_channels=None,
+                disabled_categories=None,
+                enabled_channels=None,
+                send_alert=None,
             )
             new_object.save()
 
@@ -73,7 +82,7 @@ def forward(apps: Apps, schema_editor: BaseDatabaseSchemaEditor) -> None:
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0069_documentationlink_validators'),
+        ('api', '0078_merge_20211213_0552'),
     ]
 
     operations = [
@@ -88,20 +97,21 @@ class Migration(migrations.Migration):
                 ('content', models.CharField(help_text='The definition of this filter.', max_length=100)),
                 ('description', models.CharField(help_text='Why this filter has been added.', max_length=200, null=True)),
                 ('additional_field', django.contrib.postgres.fields.jsonb.JSONField(help_text='Implementation specific field.', null=True)),
-                ('ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=20), help_text='Who to ping when this filter triggers.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field], null=True)),
+                ('ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text='Who to ping when this filter triggers.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field], null=True)),
                 ('filter_dm', models.BooleanField(help_text='Whether DMs should be filtered.', null=True)),
-                ('dm_ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=20), help_text='Who to ping when this filter triggers on a DM.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field], null=True)),
+                ('dm_ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text='Who to ping when this filter triggers on a DM.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field], null=True)),
                 ('delete_messages', models.BooleanField(help_text='Whether this filter should delete messages triggering it.', null=True)),
                 ('bypass_roles', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text='Roles and users who can bypass this filter.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_bypass_roles_field], null=True)),
                 ('enabled', models.BooleanField(help_text='Whether this filter is currently enabled.', null=True)),
                 ('dm_content', models.CharField(help_text='The DM to send to a user triggering this filter.', max_length=1000, null=True)),
+                ('dm_embed', models.CharField(help_text='The content of the DM embed', max_length=2000, null=True)),
                 ('infraction_type', models.CharField(choices=[('note', 'Note'), ('warning', 'Warning'), ('watch', 'Watch'), ('mute', 'Mute'), ('kick', 'Kick'), ('ban', 'Ban'), ('superstar', 'Superstar'), ('voice_ban', 'Voice Ban')], help_text='The infraction to apply to this user.', max_length=9, null=True)),
                 ('infraction_reason', models.CharField(help_text='The reason to give for the infraction.', max_length=1000, null=True)),
                 ('infraction_duration', models.DurationField(help_text='The duration of the infraction. Null if permanent.', null=True)),
-                ('disallowed_channels', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), null=True, size=None)),
-                ('disallowed_categories', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), null=True, size=None)),
-                ('allowed_channels', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), null=True, size=None)),
-                ('allowed_categories', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), null=True, size=None)),
+                ('disabled_channels', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text="Channels in which to not run the filter.", null=True, size=None)),
+                ('disabled_categories', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text="Categories in which to not run the filter.", null=True, size=None)),
+                ('enabled_channels', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text="Channels in which to run the filter even if it's disabled in the category.", null=True, size=None)),
+                ('send_alert', models.BooleanField(help_text='Whether an alert should be sent.', null=True)),
             ],
         ),
         migrations.CreateModel(
@@ -110,20 +120,21 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(help_text='The unique name of this list.', max_length=50)),
                 ('list_type', models.IntegerField(choices=[(1, 'Allow'), (0, 'Deny')], help_text='Whether this list is an allowlist or denylist')),
-                ('ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=20), help_text='Who to ping when this filter triggers.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field])),
+                ('ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text='Who to ping when this filter triggers.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field])),
                 ('filter_dm', models.BooleanField(help_text='Whether DMs should be filtered.')),
-                ('dm_ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=20), help_text='Who to ping when this filter triggers on a DM.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field])),
+                ('dm_ping_type', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text='Who to ping when this filter triggers on a DM.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_ping_field])),
                 ('delete_messages', models.BooleanField(help_text='Whether this filter should delete messages triggering it.')),
                 ('bypass_roles', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text='Roles and users who can bypass this filter.', size=None, validators=[pydis_site.apps.api.models.bot.filters.validate_bypass_roles_field])),
                 ('enabled', models.BooleanField(help_text='Whether this filter is currently enabled.')),
                 ('dm_content', models.CharField(help_text='The DM to send to a user triggering this filter.', max_length=1000, null=True)),
+                ('dm_embed', models.CharField(help_text='The content of the DM embed', max_length=2000, null=True)),
                 ('infraction_type', models.CharField(choices=[('note', 'Note'), ('warning', 'Warning'), ('watch', 'Watch'), ('mute', 'Mute'), ('kick', 'Kick'), ('ban', 'Ban'), ('superstar', 'Superstar'), ('voice_ban', 'Voice Ban')], help_text='The infraction to apply to this user.', max_length=9, null=True)),
                 ('infraction_reason', models.CharField(help_text='The reason to give for the infraction.', max_length=1000)),
                 ('infraction_duration', models.DurationField(help_text='The duration of the infraction. Null if permanent.', null=True)),
-                ('disallowed_channels', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), size=None)),
-                ('disallowed_categories', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), size=None)),
-                ('allowed_channels', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), size=None)),
-                ('allowed_categories', django.contrib.postgres.fields.ArrayField(base_field=models.IntegerField(), size=None)),
+                ('disabled_channels', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text="Channels in which to not run the filter.", size=None)),
+                ('disabled_categories', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text="Categories in which to not run the filter.", size=None)),
+                ('enabled_channels', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=100), help_text="Channels in which to run the filter even if it's disabled in the category.", size=None)),
+                ('send_alert', models.BooleanField(help_text='Whether an alert should be sent.')),
             ],
         ),
         migrations.AddField(
