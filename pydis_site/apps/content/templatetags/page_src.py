e3dcb3e0e965a583c1cb2257207a481800966e1a
hedy <hedy@tilde.cafe>
2022-10-29 10:40:34 +0800

    Simplify line in page_src_url template filter

--- pydis_site/apps/content/templatetags/page_src.py
+++ pydis_site/apps/content/templatetags/page_src.py
@@ -15,9 +15,6 @@ def page_src_url(request_path: str) -> str:
     For example: /pages/rules/ would return:
     https://github.com/python-discord/site/tree/main/pydis_site/apps/content/resources/rules.md
     """
-    src_url = request_path.replace(
-        "/pages/",
-        settings.CONTENT_SRC_URL,
-    )
+    src_url = request_path.replace("/pages/", settings.CONTENT_SRC_URL)
     src_url = src_url[:-1] + ".md"
     return src_url
