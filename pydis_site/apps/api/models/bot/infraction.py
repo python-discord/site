8a954029a2f0f22cde599afee3ff8195680e621e
vivekashok1221 <vivekashok1221@gmail.com>
2023-02-17 11:40:29 +0530

    Add jump_url field to infraction model

--- pydis_site/apps/api/models/bot/infraction.py
+++ pydis_site/apps/api/models/bot/infraction.py
@@ -69,6 +69,14 @@ class Infraction(ModelReprMixin, models.Model):
         help_text="Whether a DM was sent to the user when infraction was applied."
     )
 
+    jump_url = models.CharField(
+        default='',
+        max_length=88,
+        help_text=(
+            "The jump url to message invoking the infraction."
+        )
+    )
+
     def __str__(self):
         """Returns some info on the current infraction, for display purposes."""
         s = f"#{self.id}: {self.type} on {self.user_id}"
