046199eb91008ba79170b58406a7fadc43f193ab
hedy <hedy@tilde.cafe>
2024-04-06 12:56:13 +0800

    Timeline: Use pydis blurple as default icon background color

--- pydis_site/apps/timeline/apps.py
+++ pydis_site/apps/timeline/apps.py
@@ -28,14 +28,19 @@ class TimelineConfig(AppConfig):
             # Strip `.md` file extension from filename and split it into the
             # date (for sorting) and slug (for linking).
             key, slug = path.name[:-3].split("_")
+
+            icon_color = metadata.get("icon_color")
+            # Use the pydis blurple as the default background color.
+            if not icon_color or metadata["icon"] == "pydis":
+                icon_color = "has-background-primary"
+
             entry = {
                 "key": key,
                 "slug": slug,
                 "title": metadata["title"],
                 "date": metadata["date"],
                 "icon": metadata["icon"],
-                # This key might not be used if the icon uses the pydis logo.
-                "icon_color": metadata.get("icon_color"),
+                "icon_color": icon_color,
                 "content": html,
             }
 
